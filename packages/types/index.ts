667d56169e6d9882d38ef7a185b2e717b8d80b0c
Mohamed Salem <salem.ebo@gmail.com>
2020-09-18 09:41:22 +0200

    add @aurorats/types

---
+++ packages/types/index.ts
@@ -0,0 +1,104 @@
+
+declare module '*.html' {
+    const value: string;
+    export default value;
+}
+declare module '*.css' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.txt' {
+    const value: string;
+    export default value;
+}
+declare module '*.json' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.b64' {
+    const value: string;
+    export default value;
+}
+declare module '*.buf' {
+    const value: Uint8Array;
+    export default value;
+}
+
+declare module "*.apng" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.bmp" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.gif" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.ico" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.cur" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jfif" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.pjpeg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.pjp" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.svg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.tif" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.tiff" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.webp" {
+    const value: string;
+    export default value;
+}
+
+//# sourceMappingURL=static.d.ts.map
