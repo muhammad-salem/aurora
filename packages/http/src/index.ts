d0aae4b7f8fbe753bab1863e2dbed9e091543245
Mohamed Salem <salem.ebo@gmail.com>
2020-09-18 09:42:05 +0200

    add draft for http

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,3 +1,16 @@
+
+/**
+// as http request, with static, or parametrized url
+// import { getPersonList } from '/api/person.http';
+// import { getPersonInfo } from '/api/person/:id.http';
+// import { getPersonInfo } from '/api/person/{id}.http';
+// import { getPersonName } from '/api/person/{id}/name.http';
+declare module "*.http" {	
+	const value: string;	
+	export default value;
+}
+*/
+
 export class HTTPClient {
 
 }
