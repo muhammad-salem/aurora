c6f0f4edd0572feb9c1bdf864740124c9c71a058
Mohamed Salem <salem.ebo@gmail.com>
2020-10-01 12:19:53 +0200

    remove html-parser

--- packages/expression/src/index.ts
+++ packages/expression/src/index.ts
@@ -2,4 +2,3 @@ export * from './expression.js';
 export * from './operators/infix.js';
 export * from './operators/unary.js';
 export * from './parsers/js-parser.js';
-export * from './parsers/html-parser.js';
