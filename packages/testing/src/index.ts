76efabdffb917801b595c2edc8f7f61e07132697
Mohamed Salem <salem.ebo@gmail.com>
2020-10-02 08:05:43 +0200

    add indexer app

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,3 +1,24 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
+/// <reference types="@aurorats/types" />
+
+console.log('index.js loaded');
+
+import { HTMLComponent } from '@aurorats/core';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export * from './app-root/app-root-component.js';
-console.log('hi');
+
+export * from './person-app/person';
+export * from './person-app/person-app';
+
+import { AppRoot } from './app-root/app-root-component';
+
+// let appRoot = new AppRoot();
+// appRoot.appSelector = [
+//     'person-app'
+// ];
+
+let appRoot = document.getElementById('app-root') as HTMLComponent<AppRoot>;
+
+appRoot._model.appSelector = [
+    'person-app'
+];
+document.body.append(appRoot);
