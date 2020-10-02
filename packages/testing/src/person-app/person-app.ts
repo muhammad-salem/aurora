6b171a37808f9f393c793d14900af316d1266303
Mohamed Salem <salem.ebo@gmail.com>
2020-10-02 08:04:48 +0200

    add person test app

---
+++ packages/testing/src/person-app/person-app.ts
@@ -0,0 +1,41 @@
+import { Component, Input, View, HostListener } from '@aurorats/core';
+import { Person } from './person';
+import { url as templateUrl } from './person-app.html';
+
+@Component({
+    selector: 'person-app',
+    templateUrl: templateUrl
+})
+export class PersonApp {
+    @Input()
+    appVersion: string = '20.9.29';
+
+    @Input()
+    appName = 'testing framework';
+
+    @Input()
+    name = 'alice';
+
+    @View()
+    view: HTMLElement;
+
+    person1: Person = { name: 'alice', age: 39 };
+    person2: Person = { name: 'alex', age: 46 };
+    person3: Person = { name: 'delilah', age: 25 };
+    person4: Person = { name: 'alice', age: 14 };
+
+    @HostListener('person1:select')
+    onClose(data: any) {
+        console.log('AppRoot => person1:select', data);
+    }
+
+    @HostListener('personEdit:input')
+    onPersonEdit(data: any) {
+        console.log('personEdit:input', data, this.view);
+    }
+
+    @HostListener('personEdit:person.age')
+    onPersonAge(data: any) {
+        console.log('personEdit:person.age', data, this.view);
+    }
+}
---
+++ packages/testing/src/person-app/person.tsx
@@ -0,0 +1,152 @@
+import {
+    Component, EventEmitter, HostBinding, HostListener,
+    Input, JsxFactory, OnInit, Optional, Output, SelfSkip, Service,
+    View, ViewChild, ViewChildren
+} from '@aurorats/core';
+
+import { url as personViewURL } from './person-view.html';
+
+@Service({ provideIn: 'root' })
+export class LogService {
+    constructor() { }
+    info(message: string) {
+        let date = new Date();
+        console.log(
+            `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} -- ${message}`
+        );
+    }
+}
+
+export interface Person {
+    name: string;
+    age: number;
+}
+
+@Component({
+    selector: 'person-view',
+    templateUrl: personViewURL
+})
+export class PersonModel implements OnInit {
+
+    @Input() name: string;
+    @Input('age') years: number = 87;
+    @Input()
+    person: Person = {
+        name: 'Delilah',
+        age: 24
+    };
+
+    @Output() open: EventEmitter<any> = new EventEmitter();
+    @Output('select') _select: EventEmitter<any> = new EventEmitter();
+
+
+    className: string = 'p1 m1';
+
+    @View() view: HTMLElement;
+
+    @ViewChild(HTMLParagraphElement, { id: 'p-name' })
+    childName!: HTMLParagraphElement;
+
+    @ViewChild(HTMLParagraphElement, { id: 'p-age' })
+    childAge!: HTMLParagraphElement;
+
+    @ViewChildren(HTMLParagraphElement) children: HTMLParagraphElement[];
+
+
+    @HostBinding('class.valid')
+    valid: boolean;
+
+    @HostBinding('class.invalid')
+    invalid: boolean;
+
+    constructor(@Optional() private service: LogService) {
+        this.name = 'ahmed';
+    }
+
+    onInit(): void {
+        console.log('onInit', this);
+        this.open.emit('init data');
+    }
+
+    yearOfBirth() {
+        return 2020 - this.years;
+    }
+
+
+    @HostListener('window:load', ['$event'])
+    onLoad(e: Event) {
+        console.log(this, e);
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(e: Event) {
+        console.log(this, e);
+    }
+
+    @HostListener('click', ['$event.target'])
+    onClick(btn: Event) {
+        console.log('button', btn, 'number of clicks:');
+        this._select.emit(this.person);
+    }
+
+    @HostListener('select')
+    onClose(data: any) {
+        console.log('select', data);
+    }
+
+    @Input()
+    set resize(msg: string) {
+        console.log(this, msg);
+        console.log(Reflect.metadata('component', 'dd'));
+    }
+
+    collectData(@Optional() data: Object, @SelfSkip('GG') ddd: Person): string[] {
+        return [];
+    }
+}
+
+@Component({
+    selector: 'person-edit',
+    template: ({ person, printPerson }: PersonEdit) => {
+        return (
+            <form #form >
+                <input if="show" type="text" $value="$person.name" />
+                <input type="number" $value="$person.age" />
+                <input type="button" onclick={printPerson} value="Save" />
+            </form >
+        );
+    }
+})
+export class PersonEdit {
+    @Input()
+    person: Person;
+
+    @Input()
+    show = true;
+
+    @Output()
+    save = new EventEmitter<Person>();
+
+    printPerson() {
+        console.log(this.person);
+        this.save.emit(this.person);
+    }
+}
+
+
+@Component({
+    selector: 'progress-bar',
+    template: '<progress [min]="min" [max]="max" [value]="value" ></progress>'
+})
+class ProgressBar {
+
+    @Input()
+    min: number;
+
+    @Input()
+    max: number;
+
+    @Input()
+    value: number;
+
+}
