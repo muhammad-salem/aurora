5cf02defa22f18c41a5fd2d6d71bb9fc5dadf5bc
Mohamed Salem <salem.ebo@gmail.com>
2020-10-17 11:39:20 +0200

    add jsx test

---
+++ test/src/jsx-factory/intrinsic-elements.tsx
@@ -0,0 +1,9 @@
+import { Aurora } from '@aurorats/core';
+
+
+
+let x = <><div class="ddd" ><if condition=""></if></div ><svg max="3" ></svg></>;
+
+let c = <ddd class="dd"></ddd>;
+let person = { name: 'alex' };
+let directiveEle = <directive name="if" paramter="show === true" >{person.name}</directive>
