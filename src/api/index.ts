32f5243c958d423b670ef3dbe8a8dcfa140f109a
Mohamed Salem <salem.ebo@gmail.com>
2020-10-17 11:30:38 +0200

    update export

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,18 @@
 /// <reference types='@aurorats/metadata' />
 
 export * from './annotation/decorators.js';
-
 export * from './component/component.js';
 export * from './component/custom-element.js';
 export * from './component/events.js';
 export * from './component/lifecycle.js';
-
 export * from './directive/directive.js';
-
 export * from './model/change-detection.js';
-
-export * from './parser/html-template-parser.js';
-
 export * from './providers/injector.js';
 export * from './providers/provider.js';
-
 export * from './utils/elements-util.js';
 export * from './utils/observable.js';
 export * from './utils/path.js';
 export * from './utils/utils.js';
-
 export * from './view/mutation.js';
 export * from './view/render.js';
 export * from './view/view.js';
