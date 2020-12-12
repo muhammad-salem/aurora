6bba1b68ab07cbabe39fa40ba503987225e561ac
salemebo <salem.ebo@gmail.com>
2020-12-12 16:02:28 +0200

    update and optimize code

--- src/api/model/change-detection.ts
+++ src/api/model/change-detection.ts
@@ -12,94 +12,76 @@ export function isModel(object: any): object is Model {
         && object.emitChangeModel;
 }
 
-export function defineModel(object: any): void {
-    if (!isModel(object) && typeof object === 'object') {
-        const observable: { [key: string]: Function[] } = {};
-        Object.defineProperty(object, '__observable', { value: observable });
-        Object.defineProperty(object, 'subscribeModel', {
-            value: (eventName: string, callback: Function) => {
-                observable[eventName] = observable[eventName] || [];
-                observable[eventName].push(callback);
-            }
-        });
-        Object.defineProperty(object, 'emitChangeModel', {
-            value: (eventName: string, source?: any[]) => {
-                if (!source) {
-                    source = [object];
-                } else {
-                    source.push(object);
-                }
-                let calls = Object.keys(observable)
-                    .filter(key => key.startsWith(eventName) || eventName.startsWith(key));
-                calls.forEach(key => {
-                    observable[key].forEach(call => call(source));
-                });
-            }
-        });
+export function defineModel(object: any): Model {
+    if (typeof object !== 'object') {
+        throw new Error(`typeof ${typeof object} can't be a subscription model`);
     }
+    if (isModel(object)) {
+        return object;
+    }
+    const observable: { [key: string]: Function[] } = {};
+    Object.defineProperty(object, '__observable', { value: observable });
+    Object.defineProperty(object, 'subscribeModel', {
+        value: (eventName: string, callback: Function) => {
+            if (typeof callback !== 'function') {
+                return;
+            }
+            observable[eventName] = observable[eventName] || [];
+            observable[eventName].push(callback);
+        }
+    });
+    Object.defineProperty(object, 'emitChangeModel', {
+        value: (eventName: string, source?: any[]) => {
+            if (!source) {
+                source = [object];
+            } else {
+                source.push(object);
+            }
+            let calls = Object.keys(observable)
+                .filter(key => key.startsWith(eventName) || eventName.startsWith(key));
+            calls.forEach(key => {
+                observable[key].forEach(callback => callback.call(object, source));
+            });
+        }
+    });
+    return object as Model;
 }
 
-export function subscribe2way(obj1: {}, obj1PropName: string, obj2: {}, obj2PropName: string, callback1?: Function, callback2?: Function) {
+export type SourceFollwerCallback = (stack: any[]) => void;
 
-    let subject1: any, subject2: any;
-    if (isHTMLComponent(obj1)) {
-        subject1 = obj1._model;
-    } else {
-        subject1 = obj1;
-    }
-    if (isHTMLComponent(obj2)) {
-        subject2 = obj2._model;
-    } else {
-        subject2 = obj2;
-    }
+export function subscribe1way(obj1: {}, obj1PropName: string, obj2: {}, obj2PropName: string, callback?: SourceFollwerCallback) {
+    let subject1 = defineModel(isHTMLComponent(obj1) ? obj1._model : obj1);
+    let subject2 = defineModel(isHTMLComponent(obj2) ? obj2._model : obj2);
+    subject1.subscribeModel(obj1PropName, (stack: any[]) => {
+        if (callback) {
+            callback(stack);
+        }
+        // updateValue(obj1, obj1ChildName, obj2, obj2ChildName);
+        if (!stack.includes(subject2) && isModel(subject2)) {
+            subject2.emitChangeModel(obj2PropName, stack);
+        }
+    });
+}
 
-    defineModel(subject1);
-    defineModel(subject2);
-
+export function subscribe2way(obj1: {}, obj1PropName: string, obj2: {}, obj2PropName: string, callback1?: SourceFollwerCallback, callback2?: SourceFollwerCallback) {
+    let subject1 = defineModel(isHTMLComponent(obj1) ? obj1._model : obj1);
+    let subject2 = defineModel(isHTMLComponent(obj2) ? obj2._model : obj2);
     subject1.subscribeModel(obj1PropName, (source: any[]) => {
         if (callback1) {
-            callback1();
+            callback1(source);
         }
         // updateValue(obj1, obj1ChildName, obj2, obj2ChildName);
         if (!source.includes(subject2) && isModel(subject2)) {
             subject2.emitChangeModel(obj2PropName, source);
         }
     });
-    subject2.subscribeModel(obj2PropName, (source: any[]) => {
+    subject2.subscribeModel(obj2PropName, (stack: any[]) => {
         if (callback2) {
-            callback2();
+            callback2(stack);
         }
         // updateValue(obj2, obj2ChildName, obj1, obj1ChildName);
-        if (!source.includes(subject1) && isModel(subject1)) {
-            subject1.emitChangeModel(obj1PropName, source);
-        }
-    });
-}
-
-export function subscribe1way(obj1: {}, obj1PropName: string, obj2: {}, obj2PropName: string, callback?: Function) {
-
-    let subject1: any, subject2: any;
-    if (isHTMLComponent(obj1)) {
-        subject1 = obj1._model;
-    } else {
-        subject1 = obj1;
-    }
-    if (isHTMLComponent(obj2)) {
-        subject2 = obj2._model;
-    } else {
-        subject2 = obj2;
-    }
-
-    defineModel(subject1);
-    defineModel(subject2);
-
-    subject1.subscribeModel(obj1PropName, (source: any[]) => {
-        if (callback) {
-            callback();
-        }
-        // updateValue(obj1, obj1ChildName, obj2, obj2ChildName);
-        if (!source.includes(subject2) && isModel(subject2)) {
-            subject2.emitChangeModel(obj2PropName, source);
+        if (!stack.includes(subject1) && isModel(subject1)) {
+            subject1.emitChangeModel(obj1PropName, stack);
         }
     });
 }
