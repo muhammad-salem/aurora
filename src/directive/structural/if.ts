1cb038f709d9f176c211056c368f52ec81904ee1
salemebo <salem.ebo@gmail.com>
2020-12-16 20:46:46 +0200

    update IfDirective implementation

--- src/directive/structural/if.ts
+++ src/directive/structural/if.ts
@@ -1,6 +1,7 @@
-import { DirectiveNode, ElementNode } from '@aurorats/jsx';
+import { ElementNode } from '@aurorats/jsx';
 import {
-	ComponentRender, Directive, OnInit,
+	Directive, OnInit,
+	SourceFollwerCallback,
 	StructuralDirective, subscribe1way
 } from '@aurorats/api';
 import { parseJSExpression } from '@aurorats/expression';
@@ -13,43 +14,46 @@ export class IfDirective<T> extends StructuralDirective<T> implements OnInit {
 
 	condition: boolean;
 	element: HTMLElement;
-	status: boolean = false;
-
-	// constructor(
-	// 	render: ComponentRender<T>,
-	// 	comment: Comment,
-	// 	directive: DirectiveNode) {
-	// 	super(render, comment, directive);
-	// 	this.status = false;
-	// }
 
 	onInit(): void {
-		// this.status = false;
-		//TODO better api
-		console.log(`#onInit ${this.directive.directiveName}="${this.directive.directiveValue}"`);
-		this.element = this.render.createElement(this.directive.children[0] as ElementNode);
-		const propertySrc = this.render.getPropertySource(this.directive.directiveValue);
-		let expNodeDown = parseJSExpression(`model.${this.directive.directiveValue}`);
-		let context = {
-			model: propertySrc.src
-		};
-		let callback1 = () => {
-			this.condition = expNodeDown.get(context);
+		console.log(`${this.directive.directiveName}="${this.directive.directiveValue}"`);
+		let conditionNode = parseJSExpression(this.directive.directiveValue);
+		const entries = conditionNode.entry().map(key => this.render.getPropertySource(key));
+		let context = {};
+		let proxyContext = new Proxy<typeof context>(context, {
+			get(target: typeof context, p: PropertyKey, receiver: any): any {
+				const propertySrc = entries.find(src => src.property === p as string);
+				return propertySrc?.src[p];
+			},
+			set(target: typeof context, p: PropertyKey, value: any, receiver: any): boolean {
+				const propertySrc = entries.find(src => src.property === p as string);
+				return Reflect.set(propertySrc?.src, p, value);
+			}
+		});
+
+		const callback1: SourceFollwerCallback = (stack: any[]) => {
+			this.condition = conditionNode.get(proxyContext);
 			this._updateView();
+			stack.push(this);
 		};
-		subscribe1way(propertySrc.src, propertySrc.property, this, 'condition', callback1);
-		callback1();
+
+		entries.forEach(propertySrc => {
+			subscribe1way(propertySrc.src, propertySrc.property, this, 'condition', callback1);
+		});
+
+		callback1([]);
 	}
 
 	private _updateView() {
 		if (this.condition) {
-			if (!this.status) {
+			if (!this.element) {
+				this.element = this.render.createElement(this.directive.children[0] as ElementNode);
 				this.comment.after(this.element);
-				this.status = true;
 			}
-		} else {
+		}
+		else if (this.element) {
 			this.element.remove();
-			this.status = false;
+			Reflect.set(this, 'element', undefined);
 		}
 	}
 
