e4044c2e92e82af18c31701d470f588c6add8fdf
salemebo <salem.ebo@gmail.com>
2021-01-01 07:30:37 +0200

    add ContextStack

--- src/api/context/context-provider.ts
+++ src/api/context/context-provider.ts
@@ -1,80 +1,54 @@
 export type ContextDescriptorRef = { [key: string]: any };
 
-export interface ContextProvider<T> {
-    next?: ContextProvider<any>;
-    getContext(entityName: string): ContextProvider<any> | undefined;
-    getProvider(entityName: string): {} | undefined;
+export interface ContextProvider<T extends ContextDescriptorRef> {
+    hasProvider(entityName: string): boolean;
+    getProvider(entityName: string): ContextDescriptorRef;
 }
 
 export function isContextProvider<T>(obj: any): obj is ContextProvider<T> {
     return Reflect.has(obj, 'context')
-        && Reflect.has(obj.__proto__, 'getContext')
+        && Reflect.has(obj.__proto__, 'hasProvider')
         && Reflect.has(obj.__proto__, 'getProvider');
 }
 
 export class ContextProviderImpl<T extends ContextDescriptorRef> implements ContextProvider<T>{
-    constructor(public context: T, public next?: ContextProvider<any>) { }
-    getContext(entityName: string): ContextProvider<any> | undefined {
-        if (entityName in this.context) {
-            return this;
-        }
-        if (this.next) {
-            return this.next.getContext(entityName);
-        }
-        return undefined;
+    constructor(public context: T) { }
+    hasProvider(entityName: string): boolean {
+        return Reflect.has(this.context, entityName) || Reflect.has(this.context.__proto__, entityName);
     }
-    getProvider(entityName: string): {} | undefined {
-        if (entityName in this.context) {
-            return this.context[entityName];
-        }
-        if (this.next) {
-            const searchForNextContext = this.next.getContext(entityName);
-            if (searchForNextContext) {
-                return searchForNextContext.getProvider(entityName);
-            }
-        }
-        return undefined;
+    getProvider(entityName: string): ContextDescriptorRef {
+        return this.context[entityName];
     }
 }
 
-export function createContextProviders<T>(...providers: ContextDescriptorRef[]): ContextProvider<T> {
-    if (!providers || providers.length === 0) {
-        return new ContextProviderImpl(window);
+export type ContextStack<T> = Array<ContextProvider<T>> & { findContext(entityName: string): ContextProvider<T> | undefined; };
+
+export class ContextStackImpl<T extends ContextDescriptorRef> extends Array<ContextProvider<T>> implements ContextStack<T> {
+
+    findContext(entityName: string): ContextProvider<ContextDescriptorRef> | undefined {
+        return this.find(context => {
+            return context.hasProvider(entityName);
+        });
     }
-    let lastContext = new ContextProviderImpl(providers[0]);
-    for (let i = 1; i < providers.length; i++) {
-        const element = providers[i];
-        lastContext = new ContextProviderImpl(element, lastContext);
-    }
-    return lastContext;
+
 }
 
-export function appendContextProviders(lastContext: ContextProvider<ContextDescriptorRef>, ...providers: ContextDescriptorRef[]): ContextProvider<ContextDescriptorRef> {
-    for (let i = 0; i < providers.length; i++) {
-        const element = providers[i];
-        lastContext = new ContextProviderImpl(element, lastContext);
-    }
-    return lastContext;
+export function createContextStack<T extends ContextDescriptorRef>(...providers: T[]): ContextStack<T> {
+    const stack = new ContextStackImpl(providers.length);
+    providers.forEach(provider => stack.push(new ContextProviderImpl(provider)));
+    return stack;
 }
 
-export function getContext(provider: ContextDescriptorRef | ContextProvider<ContextDescriptorRef>, next?: ContextProvider<ContextDescriptorRef>): ContextProvider<ContextDescriptorRef> {
+export function getContext(provider: ContextDescriptorRef | ContextProvider<ContextDescriptorRef>): ContextProvider<ContextDescriptorRef> {
     if (isContextProvider(provider)) {
-        if (next) {
-            provider.next = next;
-        }
         return provider;
     } else {
-        return new ContextProviderImpl(provider, next);
+        return new ContextProviderImpl(provider);
     }
 }
 
-export function mergeContextProviders(...providers: (ContextDescriptorRef | ContextProvider<ContextDescriptorRef>)[]): ContextProvider<ContextDescriptorRef> {
-    if (!providers || providers.length === 0) {
-        return new ContextProviderImpl(window);
-    }
-    let lastContext = getContext(providers[0]);
-    for (let i = 1; i < providers.length; i++) {
-        lastContext = getContext(providers[i], lastContext);
-    }
-    return lastContext;
+export function mergeContextProviders<T extends ContextDescriptorRef>(...providers: (T | ContextProvider<T>)[]): ContextStack<T> {
+    const stack = new ContextStackImpl(providers.length);
+    providers.forEach(provider => stack.push(getContext(provider)));
+    return stack;
 }
