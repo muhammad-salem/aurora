b237500f9d136f85eceba438d5347a7b55794064
salemebo <salem.ebo@gmail.com>
2021-01-02 08:37:12 +0200

    add DUMMY PROXY TARGET

--- src/api/global/global-constant.ts
+++ src/api/global/global-constant.ts
@@ -4,3 +4,5 @@ import { ContextProviderImpl } from '../context/context-provider.js';
 export const WINDOW_CONTEXT_PROVIDER = new ContextProviderImpl<Window>(window);
 
 export const THIS_PROPERTY = new PropertyNode('this');
+
+export const DUMMY_PROXY_TARGET = Object.create(null);
