0551ed39c47a9678e66eb994cbbc83f6666b16e6
salemebo <salem.ebo@gmail.com>
2021-01-04 07:31:00 +0200

    add titlecase pipe

--- src/pipe/index.ts
+++ src/pipe/index.ts
@@ -1,4 +1,5 @@
 export * from './string/lowercase.pip.js';
-export * from './string/uppercase.pip.js';
+export * from './string/titlecase.pipe.js';
+export * from './string/uppercase.pipe.js';
 export * from './object/json.pipe.js';
 export * from './object/keyvalue.pipe.js';
