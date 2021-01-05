ebf50e3b80843b9011858e14bb81ac2d311e604f
salemebo <salem.ebo@gmail.com>
2021-01-05 05:19:28 +0200

    create AsyncPipeContext for every pipeline

--- src/api/pipe/pipe.ts
+++ src/api/pipe/pipe.ts
@@ -1,3 +1,4 @@
+import { defineModel, Model } from 'model/change-detection.js';
 import { ContextDescriptorRef, ContextProvider } from '../context/context-provider.js';
 import ClassRegistryProvider from '../providers/provider.js';
 
@@ -27,7 +28,7 @@ export class PipeContextProvider<T extends ContextDescriptorRef, U extends Conte
             return cachedPipe.transform.bind(cachedPipe);
         }
         const pipeRef = ClassRegistryProvider.getPipe<PipeTransform<T, U>>(entityName);
-        if (pipeRef) {
+        if (pipeRef && !pipeRef.asynchronous) {
             cachedPipe = new pipeRef.modelClass();
             this.pipeCacheMap.set(pipeRef.name, cachedPipe);
             return cachedPipe.transform.bind(cachedPipe);
@@ -38,9 +39,52 @@ export class PipeContextProvider<T extends ContextDescriptorRef, U extends Conte
         throw new Error("Pipes provider has no set implementation.");
     }
     hasContext(entityName: string): boolean {
-        return this.pipeCacheMap.has(entityName) || ClassRegistryProvider.getPipe<any>(entityName) !== undefined;
+        if (this.pipeCacheMap.has(entityName)) {
+            return true;
+        }
+        const pipeRef = ClassRegistryProvider.getPipe<PipeTransform<T, U>>(entityName);
+        return pipeRef !== undefined && !pipeRef.asynchronous;
+    }
+}
+
+export class AsyncPipeContext<T extends ContextDescriptorRef, U extends ContextDescriptorRef> implements ContextProvider<PipeTransform<T, U>> {
+    constructor(private context: PipeTransform<T, U>) { }
+    getContext(entityName: string): PipeTransform<T, U> | undefined {
+        return this.context;
+    }
+    getContextValue(entityName: string) {
+        return this.context.transform.bind(this.context);
+    }
+    setContextValue(entityName: PropertyKey, value: any): boolean {
+        throw new Error("Async Pipes provider has no set implementation.");
+    }
+    hasContext(entityName: string): boolean {
+        throw new Error("it should never been called.");
+    }
+}
+
+export class AsyncPipeContextProvider<T extends ContextDescriptorRef, U extends ContextDescriptorRef> implements ContextProvider<AsyncPipeContext<T, U>> {
+    getContext(entityName: string): AsyncPipeContext<T, U> | undefined {
+        const pipeRef = ClassRegistryProvider.getPipe<PipeTransform<T, U>>(entityName);
+        if (pipeRef && pipeRef.asynchronous) {
+            let cachedPipe: PipeTransform<T, U> | undefined;
+            cachedPipe = new pipeRef.modelClass();
+            return new AsyncPipeContext(cachedPipe);
+        }
+        return void 0;
+    }
+    getContextValue(entityName: string) {
+        return (...args: any) => { };
+    }
+    setContextValue(entityName: PropertyKey, value: any): boolean {
+        throw new Error("Async Pipes provider has no set implementation.");
+    }
+    hasContext(entityName: string): boolean {
+        const pipeRef = ClassRegistryProvider.getPipe<PipeTransform<T, U>>(entityName);
+        return pipeRef !== undefined && pipeRef.asynchronous === true;
     }
 }
 
 export const PIPE_CONTEXT_PROVIDER = new PipeContextProvider();
-export default PIPE_CONTEXT_PROVIDER;
+export const ASYNC_PIPE_CONTEXT_PROVIDER = new AsyncPipeContextProvider();
+
