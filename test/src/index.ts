179e08bf14f24c1527bba5047fde55241ba30b05
salemebo <salem.ebo@gmail.com>
2021-01-09 09:27:57 +0200

    handling views that Extending native HTML elements

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,7 +1,5 @@
 /// <reference types="@aurorats/types" />
 
-console.log('index.js loaded');
-
 import { HTMLComponent } from '@aurorats/core';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export * from './app-root/app-root-component.js';
@@ -14,16 +12,9 @@ export * from './two-way/binding-2-way';
 import { AppRoot } from './app-root/app-root-component';
 import './pipe-app/pipe-test';
 
-// let appRoot = new AppRoot();
-// appRoot.appSelector = [
-//     'person-app'
-// ];
-
-let appRoot = document.getElementById('app-root') as HTMLComponent<AppRoot>;
-
-appRoot._model.appSelector = [
+const appRoot = document.getElementById('app-root') as HTMLComponent<AppRoot>;
+appRoot._model.setAppSelector([
     'person-app',
-    'bind-2way',
+    { tag: 'div', is: 'bind-2way' },
     'pipe-app'
-];
-document.body.append(appRoot);
+]);
