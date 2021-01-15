6161c3a6ea110772d7fde233a3586bc0d0405b50
salemebo <salem.ebo@gmail.com>
2021-01-15 09:39:17 +0200

    update license

--- src/pipe/async.pipe.ts
+++ src/pipe/async.pipe.ts
@@ -1,3 +1,13 @@
+
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+
 import { Model, OnDestroy, Pipe, PipeTransform } from '@ibyar/api';
 
 interface Observer<T> {
