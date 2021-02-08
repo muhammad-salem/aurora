3662a7e7508a0a3527e70603df72fb32135c1852
salemebo <salem.ebo@gmail.com>
2021-02-08 21:53:01 +0200

    create parent class AccessNode

--- src/expression/api/definition/access.ts
+++ src/expression/api/definition/access.ts
@@ -3,38 +3,18 @@ import type { NodeDeserializer, ExpressionNode } from '../expression.js';
 import { Deserializer } from '../deserialize/deserialize.js';
 import { AbstractExpressionNode } from '../abstract.js';
 
-@Deserializer('access')
-export class PropertyAccessNode extends AbstractExpressionNode {
+export abstract class AccessNode extends AbstractExpressionNode {
 
-    static KEYWORDS = [
-        /**
-         * dot notation
-         */
-        '.',
-        /**
-         * bracket notation
-         */
-        '[]'
-    ];
-
-    static fromJSON(node: PropertyAccessNode, deserializer: NodeDeserializer): PropertyAccessNode {
-        return new PropertyAccessNode(node.op, deserializer(node.left), deserializer(node.right));
-    }
-
-    constructor(private op: string, private left: ExpressionNode, private right: ExpressionNode) {
+    constructor(protected left: ExpressionNode, protected right: ExpressionNode) {
         super();
     }
 
+    abstract get(stack: ScopedStack): any;
+    abstract toString(): string;
+    abstract event(parent?: string): string[];
+
     set(stack: ScopedStack, value: any) {
         return this.right.set(stack.stackFor(this.left.get(stack)), value);
-    }
-
-    get(stack: ScopedStack) {
-        if (this.op === '.') {
-            return this.right.get(stack.stackFor(this.left.get(stack)));
-        } else {
-            return this.left.get(stack)[this.right.get(stack)];
-        }
     }
 
     getThis(stack: ScopedStack): any {
@@ -45,30 +25,78 @@ export class PropertyAccessNode extends AbstractExpressionNode {
         return this.left.entry();
     }
 
-    event(parent?: string): string[] {
-        parent ||= '';
-        if (this.op === '.') {
-            parent += this.left.toString() + '.';
-            return this.right.event(parent);
-        } else {
-            return [`${parent}${this.left.event(parent)}[${this.right.event()}]`];
-        }
-    }
-
-    toString() {
-        if (this.op === '.') {
-            return `${this.left.toString()}.${this.right.toString()}`;
-        } else {
-            return `${this.left.toString()}[${this.right.toString()}]`;
-        }
-    }
-
     toJson(): object {
         return {
-            op: this.op,
             left: this.left.toJSON(),
             right: this.right.toJSON()
         };
+    }
+
+}
+
+
+@Deserializer('member-access')
+export class MemberAccessNode extends AccessNode {
+
+    static KEYWORDS = [
+        /**
+         * dot notation
+         */
+        '.',
+    ];
+
+    static fromJSON(node: MemberAccessNode, deserializer: NodeDeserializer): MemberAccessNode {
+        return new MemberAccessNode(deserializer(node.left), deserializer(node.right));
+    }
+
+    constructor(left: ExpressionNode, right: ExpressionNode) {
+        super(left, right);
+    }
+
+    get(stack: ScopedStack) {
+        return this.right.get(stack.stackFor(this.left.get(stack)));
+    }
+
+    event(parent?: string): string[] {
+        parent ||= '';
+        parent += this.left.toString() + '.';
+        return this.right.event(parent);
+    }
+
+    toString() {
+        return `${this.left.toString()}.${this.right.toString()}`;
+    }
+
+}
+
+@Deserializer('computed-member-access')
+export class ComputedMemberAccessNode extends AccessNode {
+
+    static KEYWORDS = [
+        /**
+         * bracket notation
+         */
+        '[', ']'
+    ];
+
+    static fromJSON(node: ComputedMemberAccessNode, deserializer: NodeDeserializer): ComputedMemberAccessNode {
+        return new ComputedMemberAccessNode(deserializer(node.left), deserializer(node.right));
+    }
+
+    constructor(left: ExpressionNode, right: ExpressionNode) {
+        super(left, right);
+    }
+
+    get(stack: ScopedStack) {
+        return this.left.get(stack)[this.right.get(stack)];
+    }
+
+    event(parent?: string): string[] {
+        return [`${parent}${this.left.event(parent)}[${this.right.event()}]`];
+    }
+
+    toString() {
+        return `${this.left.toString()}[${this.right.toString()}]`;
     }
 
 }
