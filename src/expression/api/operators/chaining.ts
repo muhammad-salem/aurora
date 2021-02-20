e6b58448e42594d78187112321c58eb0b3abaef3
salemebo <salem.ebo@gmail.com>
2021-02-20 21:43:34 +0200

    add chaining type

--- src/expression/api/operators/chaining.ts
+++ src/expression/api/operators/chaining.ts
@@ -3,25 +3,22 @@ import { AbstractExpressionNode } from '../abstract.js';
 import { Deserializer } from '../deserialize/deserialize.js';
 import { ScopedStack } from '../scope.js';
 
+export type ChainingType = 'access' | 'computed' | 'function';
 @Deserializer('chaining')
 export class OptionalChainingNode extends AbstractExpressionNode {
 
 	static fromJSON(node: OptionalChainingNode, deserializer: NodeDeserializer): OptionalChainingNode {
-		return new OptionalChainingNode(deserializer(node.optional), deserializer(node.property));
+		return new OptionalChainingNode(deserializer(node.optional), deserializer(node.property), node.type);
 	}
 
 	static KEYWORDS = ['?.'];
 
-	constructor(private optional: ExpressionNode, private property: ExpressionNode) {
+	constructor(private optional: ExpressionNode, private property: ExpressionNode, private type: ChainingType) {
 		super();
 	}
 
 	set(stack: ScopedStack, value: any) {
-		const object = this.optional.get(stack);
-		if (object === null || object === undefined) {
-			return undefined
-		}
-		return this.property.set(stack.stackFor(object), value);
+		throw new Error(`OptionalChainingNode.#set() has no implementation.`)
 	}
 
 	get(stack: ScopedStack) {
@@ -29,7 +26,14 @@ export class OptionalChainingNode extends AbstractExpressionNode {
 		if (object === null || object === undefined) {
 			return undefined
 		}
-		return this.property.get(object);
+		switch (this.type) {
+			case 'access':
+				return this.property.get(stack.stackFor(object));
+			case 'computed':
+				return object[this.property.get(stack)];
+			case 'function':
+				return this.property.get(stack.stackFor(object));
+		}
 	}
 
 	getThis(stack: ScopedStack): any {
@@ -51,7 +55,8 @@ export class OptionalChainingNode extends AbstractExpressionNode {
 	toJson(): object {
 		return {
 			optional: this.optional.toJSON(),
-			property: this.property.toJSON()
+			property: this.property.toJSON(),
+			type: this.type
 		};
 	}
 
