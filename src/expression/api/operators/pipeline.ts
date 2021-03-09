9a5816e22e9d687f829b95d7229682ad7ab38148
salemebo <salem.ebo@gmail.com>
2021-03-09 07:36:04 +0200

    do optimization on pipeline

--- src/expression/api/operators/pipeline.ts
+++ src/expression/api/operators/pipeline.ts
@@ -17,12 +17,15 @@ export class PipelineNode extends AbstractExpressionNode {
 		return new PipelineNode(
 			deserializer(node.param),
 			deserializer(node.func),
-			node.args?.map(deserializer)
+			node.args?.map(deserializer),
+			node.index
 		);
 	}
 	static KEYWORDS = ['|>', ':', '?', ','];
+	private argumentList: ExpressionNode[];
 	constructor(private param: ExpressionNode, private func: ExpressionNode, private args: ExpressionNode[] = [], private index = 0) {
 		super();
+		this.argumentList = this.args.slice().splice(this.index, 0, this.param);
 	}
 	set(stack: ScopedStack, value: any) {
 		throw new Error(`PipelineNode#set() has no implementation.`);
@@ -30,11 +33,7 @@ export class PipelineNode extends AbstractExpressionNode {
 	get(stack: ScopedStack) {
 		const funCallBack = this.func.get(stack) as Function;
 		const thisContext = this.func.getThis?.(stack);
-		if (this.index) {
-			const args = this.args.splice(this.index, 0, this.param);
-			return funCallBack.call(thisContext, ...args.map(arg => arg.get(stack)));
-		}
-		return funCallBack.call(thisContext, this.param.get(stack), ...this.args.map(arg => arg.get(stack)));
+		return funCallBack.call(thisContext, ...this.argumentList.map(arg => arg.get(stack)));
 	}
 	entry(): string[] {
 		return [...this.func.entry(), ...this.param.entry(), ...this.args.flatMap(arg => arg.entry())];
@@ -49,7 +48,8 @@ export class PipelineNode extends AbstractExpressionNode {
 		return {
 			param: this.param.toJSON(),
 			func: this.func.toJSON(),
-			args: this.args?.map(arg => arg.toJSON())
+			args: this.args?.map(arg => arg.toJSON()),
+			index: this.index
 		};
 	}
 }
