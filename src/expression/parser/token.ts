11edf68f92fca9b2eee15367b5e84a87522f36e6
salemebo <salem.ebo@gmail.com>
2021-03-11 07:39:31 +0200

    check token type

--- src/expression/parser/token.ts
+++ src/expression/parser/token.ts
@@ -10,6 +10,9 @@ export enum TokenType {
 	BIGINT = 'BIGINT',
 	PROPERTY = 'PROPERTY',
 
+	OBJECT = 'OBJECT',
+	ARRAY = 'ARRAY',
+
 	OPEN_PARENTHESES = 'OPEN_PARENTHESES',
 	CLOSE_PARENTHESES = 'CLOSE_PARENTHESES',
 	OPEN_BRACKETS = 'OPEN_BRACKETS',
@@ -23,9 +26,6 @@ export enum TokenType {
 	OPERATOR = 'OPERATOR',
 	STATEMENT = 'STATEMENT',
 
-	OBJECT = 'OBJECT',
-	ARRAY = 'ARRAY',
-
 	EOF = 'EOF',
 	NS = 'NOT_SUPPORTED',
 
@@ -35,17 +35,62 @@ export enum TokenType {
 
 export class Token {
 	constructor(public type: TokenType, public value: string | ExpressionNode) { }
-	getExpressionNode(): ExpressionNode {
+	valueAsExpressionNode(): ExpressionNode {
 		if (this.type !== TokenType.EXPRESSION) {
 			throw new Error(`can't convert to ExpressionNode`);
 		}
 		return this.value as ExpressionNode;
 	}
-	asString(): string {
+	valueAsString(): string {
 		if (typeof this.value !== 'string') {
 			throw new Error(`can't convert to string`);
 		}
 		return this.value as string;
+	}
+	isValue(): boolean {
+		switch (this.type) {
+			case TokenType.STRING:
+			case TokenType.NUMBER:
+			case TokenType.BOOLEAN:
+			case TokenType.NULLISH:
+			case TokenType.REGEXP:
+			case TokenType.BIGINT:
+			case TokenType.PROPERTY:
+			case TokenType.OBJECT:
+			case TokenType.ARRAY:
+				return true;
+			default:
+				return false;
+		}
+	}
+	isEndOfStatement(): boolean {
+		switch (this.type) {
+			case TokenType.SEMICOLON:
+			case TokenType.COMMA:
+			case TokenType.CLOSE_PARENTHESES:
+			case TokenType.CLOSE_BRACKETS:
+			case TokenType.CLOSE_CURLY:
+			case TokenType.EOF:
+				return true;
+			default:
+				return false;
+		}
+	}
+	isOperator(): boolean {
+		switch (this.type) {
+			case TokenType.OPERATOR:
+				return true;
+			default:
+				return false;
+		}
+	}
+	isStatement(): boolean {
+		switch (this.type) {
+			case TokenType.STATEMENT:
+				return true;
+			default:
+				return false;
+		}
 	}
 	toString(): string {
 		return this.type + ': ' + String(this.value);
