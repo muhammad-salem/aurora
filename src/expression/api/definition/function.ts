ffee2bdc4a7c9c84941d99a9bc517765245b23ab
salemebo <salem.ebo@gmail.com>
2021-04-08 02:00:43 +0200

    edit declare function

--- src/expression/api/definition/function.ts
+++ src/expression/api/definition/function.ts
@@ -4,21 +4,31 @@ import { AbstractExpressionNode } from '../abstract.js';
 import { Deserializer } from '../deserialize/deserialize.js';
 import { PropertyNode } from './values.js';
 
-@Deserializer('function-declaration')
+export enum FunctionType {
+	NORMAL = 'NORMAL',
+	GENERATOR = 'GENERATOR',
+	ASYNC = 'ASYNC'
+}
+export enum ArrowFunctionType {
+	NORMAL = 'NORMAL',
+	ASYNC = 'ASYNC'
+}
+
+@Deserializer('function')
 export class FunctionDeclarationNode extends AbstractExpressionNode {
-	static KEYWORDS = ['function', '=>'];
+	static KEYWORDS = ['function'];
 	static fromJSON(node: FunctionDeclarationNode, deserializer: NodeDeserializer): FunctionDeclarationNode {
 		return new FunctionDeclarationNode(
 			deserializer(node.parameters),
 			deserializer(node.statements),
-			node.isArrow,
+			FunctionType[node.type],
 			node.name ? deserializer(node.name) as PropertyNode : void 0
 		);
 	}
 	constructor(
 		private parameters: ExpressionNode,
 		private statements: ExpressionNode,
-		private isArrow: boolean = false,
+		private type: FunctionType,
 		private name?: ExpressionNode) {
 		super();
 	}
@@ -28,8 +38,8 @@ export class FunctionDeclarationNode extends AbstractExpressionNode {
 	getStatements() {
 		return this.statements;
 	}
-	getIsArrow() {
-		return this.isArrow;
+	getType() {
+		return this.type;
 	}
 	getName() {
 		return this.name;
@@ -38,11 +48,32 @@ export class FunctionDeclarationNode extends AbstractExpressionNode {
 		throw new Error('FunctionDeclarationNode#set() has no implementation.');
 	}
 	get(stack: ScopedStack) {
-		const func = (...args: any[]) => {
-			const funcStack = stack.newStack();
-			this.parameters.set(funcStack, args);
-			return this.statements.get(funcStack);
-		};
+		const self = this;
+		let func: Function;
+		switch (this.type) {
+			case FunctionType.ASYNC:
+				func = async function (...args: any[]) {
+					const funcStack = stack.newStack();
+					self.parameters.set(funcStack, args);
+					return self.statements.get(funcStack);
+				};
+				break;
+			case FunctionType.GENERATOR:
+				func = function* (...args: any[]) {
+					const funcStack = stack.newStack();
+					self.parameters.set(funcStack, args);
+					return self.statements.get(funcStack);
+				};
+				break; break;
+			default:
+			case FunctionType.NORMAL:
+				func = function (...args: any[]) {
+					const funcStack = stack.newStack();
+					self.parameters.set(funcStack, args);
+					return self.statements.get(funcStack);
+				};
+				break;
+		}
 		if (this.name) {
 			this.name.set(stack, func);
 		}
@@ -59,23 +90,90 @@ export class FunctionDeclarationNode extends AbstractExpressionNode {
 		return this.statements.event();
 	}
 	toString(): string {
-		if (this.isArrow) {
-			return `${this.parameters.toString()} => {
-                ${this.statements.toString()}
-            }`;
-		} else {
-			return `function ${this.name?.toString() || ''}${this.parameters.toString()} {
-                ${this.statements.toString()}
-            }`;
+		let declare: string;
+		switch (this.type) {
+			case FunctionType.ASYNC:
+				declare = 'async function'; break;
+			case FunctionType.GENERATOR:
+				declare = 'function*'; break;
+			default:
+			case FunctionType.NORMAL:
+				declare = 'function'; break;
 		}
+		return `${declare} ${this.name?.toString() || ''}(${this.parameters.toString()}) ${this.statements.toString()}`;
 	}
 	toJson(): object {
-		const func = {
+		return {
 			parameters: this.parameters.toJSON(),
 			statements: this.statements.toJSON(),
-			isArrow: this.isArrow,
+			type: this.type,
 			name: this.name?.toJSON()
 		};
+	}
+}
+
+
+@Deserializer('=>')
+export class ArrowFunctionNode extends AbstractExpressionNode {
+	static KEYWORDS = ['=>'];
+	static fromJSON(node: ArrowFunctionNode, deserializer: NodeDeserializer): ArrowFunctionNode {
+		return new ArrowFunctionNode(
+			deserializer(node.parameters),
+			deserializer(node.statements),
+			ArrowFunctionType[node.type]
+		);
+	}
+	constructor(private parameters: ExpressionNode, private statements: ExpressionNode, private type: ArrowFunctionType) {
+		super();
+	}
+	getParameters() {
+		return this.parameters;
+	}
+	getStatements() {
+		return this.statements;
+	}
+	set(stack: ScopedStack, value: Function) {
+		throw new Error('FunctionDeclarationNode#set() has no implementation.');
+	}
+	get(stack: ScopedStack) {
+		let func: Function;
+		switch (this.type) {
+			case ArrowFunctionType.NORMAL:
+				func = (...args: any[]) => {
+					const funcStack = stack.newStack();
+					this.parameters.set(funcStack, args);
+					return this.statements.get(funcStack);
+				};
+				break;
+			default:
+			case ArrowFunctionType.NORMAL:
+				func = async (...args: any[]) => {
+					const funcStack = stack.newStack();
+					this.parameters.set(funcStack, args);
+					return this.statements.get(funcStack);
+				};
+				break;
+		}
 		return func;
+	}
+	entry(): string[] {
+		return [
+			...this.parameters.entry(),
+			/** remove for now, should return only object not defined in this function scope */
+			// ...this.funcBody.flatMap(line => line.entry())
+		];
+	}
+	event(): string[] {
+		return this.statements.event();
+	}
+	toString(): string {
+		return `${this.type === ArrowFunctionType.ASYNC ? 'async ' : ''}${this.parameters.toString()} => ${this.statements.toString()}`;
+	}
+	toJson(): object {
+		return {
+			parameters: this.parameters.toJSON(),
+			statements: this.statements.toJSON(),
+			type: this.type,
+		};
 	}
 }
