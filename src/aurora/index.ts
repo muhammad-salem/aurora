dc7a3d80a8a183c6dabdb59fbd150657f109bb40
salemebo <salem.ebo@gmail.com>
2021-04-29 14:18:42 +0200

    remove empty package for now

--- src/aurora/index.ts
+++ src/aurora/index.ts
@@ -1,5 +1,3 @@
-/// <reference types="@ibyar/types" />
-
 export * from '@ibyar/element';
 export * from '@ibyar/expression';
 export * from '@ibyar/metadata';
@@ -8,6 +6,3 @@ export * from '@ibyar/html-parser';
 export * from '@ibyar/api';
 export * from '@ibyar/directives';
 export * from '@ibyar/pipes';
-export * from '@ibyar/forms';
-export * from '@ibyar/router';
-export * from '@ibyar/http';
