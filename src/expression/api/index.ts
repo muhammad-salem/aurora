5aac16bc03e2779e3501fc7726a971cc722e4118
salemebo <salem.ebo@gmail.com>
2021-04-29 21:07:58 +0200

    add index file

---
+++ src/expression/api/index.ts
@@ -0,0 +1,50 @@
+export * from './expression.js';
+export * from './scope.js';
+export * from './abstract.js';
+
+export * from './deserialize/deserialize.js';
+export * from './context/provider.js';
+
+export * from './definition/values.js';
+export * from './definition/array.js';
+export * from './definition/comment.js';
+export * from './definition/function.js';
+export * from './definition/member.js';
+export * from './definition/object.js';
+export * from './definition/statement.js';
+
+
+export * from './computing/function.js';
+export * from './computing/new.js';
+export * from './computing/return.js';
+export * from './computing/spread.js';
+export * from './computing/throw.js';
+
+
+export * from './operators/arithmetic.js';
+export * from './operators/assignment.js';
+export * from './operators/chaining.js';
+export * from './operators/comma.js';
+export * from './operators/destruct.js';
+export * from './operators/equality.js';
+export * from './operators/grouping.js';
+export * from './operators/logical.js';
+export * from './operators/pipeline.js';
+export * from './operators/relational.js';
+export * from './operators/shift.js';
+export * from './operators/ternary.js';
+export * from './operators/types.js';
+export * from './operators/unary.js';
+
+
+export * from './statement/controlflow/block.js';
+export * from './statement/controlflow/empty.js';
+export * from './statement/controlflow/if.js';
+export * from './statement/controlflow/switch.js';
+export * from './statement/controlflow/terminate.js';
+
+
+export * from './statement/declarations/declares.js';
+
+export * from './statement/iterations/for.js';
+export * from './statement/iterations/while.js';
