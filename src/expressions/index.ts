8b8485f9259dae5ce50c5d38b4f7229e0d0dfd82
salemebo <salem.ebo@gmail.com>
2021-05-03 20:08:26 +0200

    remove old expression api

--- src/expressions/index.ts
+++ src/expressions/index.ts
@@ -1,10 +1,2 @@
-export * from './expression.js';
-export * from './operators/infix.js';
-export * from './operators/unary.js';
-export * from './parsers/parser.js';
-export * from './parsers/js-parser.js';
-export * from './statement/for-expr.js';
-
-// export * from './api/index.js';
-
+export * from './api/index.js';
 export * from './v8/index.js';
