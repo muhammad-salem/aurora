e6356d8cf361496b805f1e39d987bb76b78e2a8b
salemebo <salem.ebo@gmail.com>
2021-05-03 20:40:19 +0200

    remove old modules

--- src/core/global/global-constant.ts
+++
@@ -1,8 +0,0 @@
-import { PropertyNode } from '@ibyar/expressions';
-import { ContextProviderImpl } from '../context/context-provider.js';
-
-export const WINDOW_CONTEXT_PROVIDER = new ContextProviderImpl<Window>(window);
-
-export const THIS_PROPERTY = new PropertyNode('this');
-
-export const DUMMY_PROXY_TARGET = Object.create(null);
