a750350db8d3bd07af8d856e86c3c49b4a5c6075
salemebo <salem.ebo@gmail.com>
2021-05-12 14:14:13 +0200

    add BindingContextProvider

--- src/core/context/stack.ts
+++ src/core/context/stack.ts
@@ -1,4 +1,4 @@
-import { ScopeProvider, ReadOnlyScopedContext } from '@ibyar/expressions';
+import { ScopeProvider, ReadOnlyScopedContext, ScopedContext } from '@ibyar/expressions';
 import { PipeProvider, AsyncPipeProvider } from '../pipe/pipe.js';
 
 const Constant: { [k: string]: any } = {
@@ -73,3 +73,43 @@ const Constant: { [k: string]: any } = {
 
 const readOnlyProvider = new ReadOnlyScopedContext(Constant);
 export const documentStack = new ScopeProvider([readOnlyProvider, new PipeProvider(), new AsyncPipeProvider()]);
+
+export class BindingNotification {
+	notification: Array<PropertyKey> = [];
+	notify(propertyKey: PropertyKey) {
+		this.notification.push(propertyKey);
+		console.log(this.notification);
+	}
+}
+export class BindingContextProvider<T extends object> implements ScopedContext {
+
+	static contextMapping = new WeakMap<object, BindingContextProvider<object>>();
+
+	static createOrGetProvider<T extends object>(object: T): BindingContextProvider<T> {
+		if (this.contextMapping.has(object)) {
+			return this.contextMapping.get(object) as BindingContextProvider<T>;
+		}
+		const notifier = new BindingNotification();
+		const provider = new BindingContextProvider(object, notifier);
+		this.contextMapping.set(object, provider);
+		return provider;
+	}
+	constructor(protected context: T, private notifier: BindingNotification) { }
+	getNotifier() {
+		return this.notifier;
+	}
+	get(propertyKey: PropertyKey): any {
+		return Reflect.get(this.context, propertyKey);
+	}
+	set(propertyKey: PropertyKey, value: any, receiver?: any): boolean {
+		const hasSet = Reflect.set(this.context, propertyKey, value);
+		this.notifier.notify(propertyKey);
+		return hasSet;
+	}
+	has(propertyKey: PropertyKey): boolean {
+		return propertyKey in this.context;
+	}
+	getProviderBy(): T | undefined {
+		return this.context;
+	}
+}
