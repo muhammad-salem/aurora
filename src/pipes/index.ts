3fe08ad2ea627b2764ecf38679d463cd0f69b979
salemebo <salem.ebo@gmail.com>
2021-08-11 02:24:41 +0200

    split object pipes into individual file

--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,8 +1,13 @@
+// asynchronous
+export * from './async.pipe.js';
+
 // array pipes
 export * from './array/slice.pipe.js';
 
-export * from './async.pipe.js';
-export * from './objects.pipe.js';
+// object pipes
+export * from './object/json.pipe.js';
+export * from './object/key-value.pipe.js';
+export * from './object/to-json.pipe.js';
 
 // string pipes
 export * from './string/lower-case.pipe.js';
