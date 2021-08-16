db1cdfab950cc099bf5c0ad83ff86509b64d750d
salemebo <salem.ebo@gmail.com>
2021-08-17 00:32:18 +0200

    update BOOTSTRAP_SYMBOL

--- src/metadata/bootstrap-data.ts
+++ src/metadata/bootstrap-data.ts
@@ -1,21 +1,33 @@
 
+const BOOTSTRAP_SYMBOL = Symbol.for('bootstrap');
+
 export function findByModelClassOrCreat<T = any>(modelProperty: Object): T {
-	var bootstrapMetadata: T = Reflect.get(modelProperty, 'bootstrap');
+	var bootstrapMetadata: T = Reflect.get(modelProperty, BOOTSTRAP_SYMBOL);
 	if (!bootstrapMetadata) {
-		bootstrapMetadata = {} as T;
-		Object.defineProperty(modelProperty, 'bootstrap', { value: bootstrapMetadata });
+		bootstrapMetadata = Object.create(null) as T;
+		Reflect.set(modelProperty, BOOTSTRAP_SYMBOL, bootstrapMetadata);
 	}
 	return bootstrapMetadata;
 }
 
-export function setBootstrapTagNameMetadata(modelProperty: Object, bootstrapTagName: string, metadata: Object) {
-	Reflect.set(modelProperty, bootstrapTagName, metadata);
+export function setBootstrapMetadata(modelProperty: Object, metadata: Object) {
+	Reflect.set(modelProperty, BOOTSTRAP_SYMBOL, metadata);
 }
 
 export function getBootstrapMetadata<T = any>(modelProperty: Object): T {
-	return Reflect.get(modelProperty, 'bootstrap');
+	return Reflect.get(modelProperty, BOOTSTRAP_SYMBOL);
 }
 
-export function getBootstrapTagNameMetadata<T = any>(modelProperty: Object, bootstrapTagName: string): T {
-	return Reflect.get(modelProperty, bootstrapTagName);
+export function hasBootstrapMetadata<T = any>(modelProperty: Object): boolean {
+	return Reflect.has(modelProperty, BOOTSTRAP_SYMBOL);
+}
+
+export function deleteBootstrapMetadata<T = any>(modelProperty: Object): boolean {
+	return Reflect.deleteProperty(modelProperty, BOOTSTRAP_SYMBOL);
+}
+
+export function getAndDeleteBootstrapMetadata<T = any>(modelProperty: Object): T {
+	const bootstrap = Reflect.get(modelProperty, BOOTSTRAP_SYMBOL);
+	Reflect.deleteProperty(modelProperty, BOOTSTRAP_SYMBOL);
+	return bootstrap;
 }
