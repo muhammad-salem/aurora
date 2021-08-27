7ec421f20eb1a1007fa93d10d7d083e48080f18f
salemebo <salem.ebo@gmail.com>
2021-08-28 01:34:07 +0200

    update example

--- test/expressions/expression.spec.mjs
+++ test/expressions/expression.spec.mjs
@@ -1,11 +1,29 @@
-import { JavaScriptParser, Stack } from '@ibyar/expressions';
+import { JavaScriptParser, Scope, Stack } from '@ibyar/expressions';
 
 const source = `
-	let { x, y } = {x: 5, y: 99};
-	console.log(x, y);
-	`;
+	let { x, y } = {x: 5, y: 55};
+	const z = x / y;
+	console.log({x, y, z});
+	const alex = { firstName: 'alex', lastName: 'jon', age: 30 };
+	alex.fullName = function(){ return this.firstName + ' ' + this.lastName;};
+	console.log(alex.fullName());
+	console.log(alex.fullName.toString());
+	setTimeout(() => console.log('setTimeout', alex), 500);
+	const sara = { firstName: 'sara', lastName: 'jon', age: 28, [Symbol.toStringTag]: 'SARA'};
+	console.log(sara['age']);
+	console.log('toStringTag', sara[Symbol.toStringTag]);
+	console.log('compare', sara.age <=> alex.age);
+	console.log('older', sara.age >? alex.age);
+	console.log('younger', sara.age <? alex.age);
+	console.log('typeof', typeof alex);
+	console.log('typeof', typeof alex.age);
+	`
+	+
+	'let stringLiteralExample = `${alex.firstName} and ${sara.firstName} are friends`; console.log({stringLiteralExample})';
 const ast = JavaScriptParser.parse(source);
-console.log(JSON.stringify(ast, void 0, 2));
+const esTree = ast.toJSON();
+const esTreeString = JSON.stringify(ast, void 0, 2);
+console.log({ esTree, esTreeString });
 const context = {
 	Promise,
 	setTimeout,
@@ -13,5 +31,14 @@ const context = {
 	Symbol
 };
 const stack = Stack.for(context);
+const globalScope = Scope.emptyFunctionScope();
+stack.pushScope(globalScope);
 ast.get(stack);
-console.log(context.x, context.y);
+console.log(
+	'from global context values: x: %s, y: %s',
+	globalScope.getContext().x,
+	globalScope.getContext().y
+);
+console.log(
+	globalScope.getContext()
+);
