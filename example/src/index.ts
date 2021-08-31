f1d81006b19333b543891ce9e91d5a8bac8c8a10
salemebo <salem.ebo@gmail.com>
2021-08-31 04:43:28 +0200

    add video app

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,12 +4,14 @@ import { HTMLComponent } from '@ibyar/aurora';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@popperjs/core';
-export * from './app-root/app-root-component.js';
+export * from './app-root/app-root-component';
 
 export * from './person-app/person';
 export * from './person-app/person-app';
 
 export * from './two-way/binding-2-way';
+
+export * from './video-player/video';
 
 import { AppRoot } from './app-root/app-root-component';
 import './pipe-app/pipe-test';
@@ -18,5 +20,6 @@ const appRoot = document.getElementById('app-root') as HTMLComponent<AppRoot>;
 appRoot._model.setAppSelector([
 	'person-app',
 	{ tag: 'div', is: 'bind-2way' },
-	'pipe-app'
+	'pipe-app',
+	'video-play-list'
 ]);
---
+++ example/src/video-player/video.ts
@@ -0,0 +1,48 @@
+import { Component, isModel, OnInit, View } from '@ibyar/aurora';
+
+@Component({
+	selector: 'video-player',
+	extend: 'video',
+	encapsulation: 'shadow-dom', // will be ignored 'video' element can't attach shadow dom, it already applied before
+	shadowDomMode: 'open', // will be ignored
+	shadowDomDelegatesFocus: true // will be ignored
+})
+export class VideoPlayer implements OnInit {
+	@View()
+	player: HTMLVideoElement;
+
+	onInit() {
+		console.log('src', this.player.src);
+	}
+}
+
+@Component({
+	selector: 'video-play-list',
+	template: `
+	<div class="row">
+		<div class="col-12" *for="let fileName of names">
+			<a href="#" (click)="playVideo(fileName)">{{fileName}}</a>
+		</div>
+	</div>
+	<video-player *if="file; else noMedia" controls autoplay name="media">
+		<source [src]="file" type="video/mp4" />
+	</video-player>
+	<template #noMedia>No Video Source Found</template>
+	`
+})
+export class VideoPlayList {
+
+	names: string[] = [
+		'http://github.com/mediaelement/mediaelement-files/blob/master/big_buck_bunny.mp4?raw=true',
+	];
+	file: string | undefined = undefined;
+
+	playVideo(fileName: string) {
+		this.file = fileName;
+		console.log(fileName);
+		if (isModel(this)) {
+			this.emitChangeModel('file');
+		}
+	}
+
+}
