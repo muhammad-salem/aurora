70c65e3c0c8e7b2fc698fcefcaea18511f85ac11
salemebo <salem.ebo@gmail.com>
2021-09-04 17:55:08 +0200

    define Classes

--- src/expressions/api/app/class.ts
+++ src/expressions/api/app/class.ts
@@ -1,30 +1,152 @@
 import type { Stack } from '../../scope/stack.js';
+import type { CanDeclareVariable, ExpressionNode, NodeDeserializer } from '../expression.js';
 import { AbstractExpressionNode } from '../abstract.js';
 import { Deserializer } from '../deserialize/deserialize.js';
-import type { ExpressionNode, NodeDeserializer } from '../expression.js';
+import { FunctionExpression, Identifier } from '../index.js';
+import { ScopeType } from '../../scope/scope.js';
 
 
-@Deserializer('class')
-export class ClassNode extends AbstractExpressionNode {
-	static fromJSON(node: ClassNode, deserializer: NodeDeserializer): ClassNode {
-		return new ClassNode(
-			node.body.map(deserializer),
-			node.className ? deserializer(node.className) : void 0,
-			node.extendsClass ? deserializer(node.extendsClass) : void 0
+/**
+ * MetaProperty node represents
+ * - `new.target` meta property in ES2015.
+ * - `import.meta` meta property in ES2030.
+ * 
+ * In the future, it will represent other meta properties as well.
+ */
+@Deserializer('MetaProperty')
+export class MetaProperty extends AbstractExpressionNode {
+	static fromJSON(node: MetaProperty, deserializer: NodeDeserializer<any>): MetaProperty {
+		return new MetaProperty(
+			deserializer(node.meta),
+			deserializer(node.property)
 		);
 	}
+
+	constructor(private meta: Identifier, private property: Identifier) {
+		super();
+	}
+	set(stack: Stack, value: any) {
+		throw new Error('Method not implemented.');
+	}
+	get(stack: Stack, thisContext?: any) {
+		throw new Error('Method not implemented.');
+	}
+	entry(): string[] {
+		throw new Error('Method not implemented.');
+	}
+	event(parent?: string): string[] {
+		throw new Error('Method not implemented.');
+	}
+	toString(): string {
+		throw new Error('Method not implemented.');
+	}
+	toJson(key?: string): { [key: string]: any; } {
+		return {
+			meta: this.meta.toJSON(),
+			property: this.property.toJSON(),
+		};
+	}
+}
+
+export type MethodDefinitionKind = 'constructor' | 'method' | 'set' | 'get';
+
+@Deserializer('MethodDefinition')
+export class MethodDefinition extends AbstractExpressionNode {
+	static fromJSON(node: MethodDefinition, deserializer: NodeDeserializer<any>): MethodDefinition {
+		return new MethodDefinition(
+			node.kind,
+			deserializer(node.key),
+			deserializer(node.value),
+			node.computed,
+			node.static
+		);
+	}
+
+	private 'static': boolean;
+
 	constructor(
-		private body: ExpressionNode[],
-		private className?: ExpressionNode,
-		private extendsClass?: ExpressionNode) {
+		private kind: MethodDefinitionKind,
+		private key: ExpressionNode,
+		private value: FunctionExpression,
+
+		private computed: boolean,
+		isStatic: boolean) {
+		super();
+		this.static = isStatic;
+	}
+	set(stack: Stack, value: any) {
+		throw new Error('Method not implemented.');
+	}
+	get(stack: Stack, thisContext?: any) {
+		throw new Error('Method not implemented.');
+	}
+	entry(): string[] {
+		throw new Error('Method not implemented.');
+	}
+	event(parent?: string): string[] {
+		throw new Error('Method not implemented.');
+	}
+	toString(): string {
+		throw new Error('Method not implemented.');
+	}
+	toJson(key?: string): { [key: string]: any; } {
+		return {
+			kind: this.kind,
+			key: this.key,
+			value: this.value,
+			computed: this.computed,
+			static: this.static,
+		};
+	}
+}
+
+
+@Deserializer('ClassBody')
+export class ClassBody extends AbstractExpressionNode {
+	static fromJSON(node: ClassBody, deserializer: NodeDeserializer<any>): ClassBody {
+		return new ClassBody(
+			node.body.map(deserializer)
+		);
+	}
+
+	constructor(private body: MethodDefinition[]) {
+		super();
+	}
+	set(stack: Stack, value: any) {
+		throw new Error('Method not implemented.');
+	}
+	get(stack: Stack, thisContext?: any) {
+		throw new Error('Method not implemented.');
+	}
+	entry(): string[] {
+		throw new Error('Method not implemented.');
+	}
+	event(parent?: string): string[] {
+		throw new Error('Method not implemented.');
+	}
+	toString(): string {
+		throw new Error('Method not implemented.');
+	}
+	toJson(key?: string): { [key: string]: any; } {
+		return {
+			body: this.body.map(method => method.toJSON())
+		};
+	}
+}
+
+export class Class extends AbstractExpressionNode {
+	constructor(
+		protected body: ClassBody,
+		protected id?: Identifier,
+		protected superClass?: ExpressionNode) {
 		super();
 	}
 
 	set(stack: Stack) {
-		throw new Error(`ClassNode.#set() has no implementation.`);
+		throw new Error(`Class.#set() has no implementation.`);
 	}
 	get(stack: Stack) {
-		throw new Error(`ClassNode.#get() has no implementation.`);
+		throw new Error(`Class.#get() has no implementation.`);
 	}
 	entry(): string[] {
 		return [];
@@ -33,23 +155,47 @@ export class ClassNode extends AbstractExpressionNode {
 		return [];
 	}
 	toString() {
-		let classDeclaration = '';
-		if (this.className) {
-			classDeclaration += this.className.toString();
+		let classDeclaration = 'class ';
+		if (this.id) {
+			classDeclaration += this.id.toString();
 		}
-		if (this.extendsClass) {
-			if (this.className) {
-				classDeclaration += ' ';
-			}
-			classDeclaration += 'extends ' + this.extendsClass.toString();
+		if (this.superClass) {
+			classDeclaration += ' extends ' + this.superClass.toString();
 		}
-		return `class ${classDeclaration} {\n\t${this.body.map(expr => expr.toString()).join(';\n\t')}}\n`;
+		return `${classDeclaration} {${this.body.toString()}\n`;
 	}
 	toJson(): object {
 		return {
-			body: this.body.map(expression => expression.toJSON()),
-			className: this.className?.toJSON(),
-			extendsClass: this.extendsClass?.toJSON(),
+			body: this.body.toJSON(),
+			id: this.id?.toJSON(),
+			superClass: this.superClass?.toJSON(),
 		};
 	}
 }
+
+@Deserializer('ClassDeclaration')
+export class ClassDeclaration extends Class implements CanDeclareVariable {
+	static fromJSON(node: ClassDeclaration, deserializer: NodeDeserializer<any>): ClassDeclaration {
+		return new ClassDeclaration(
+			deserializer(node.body),
+			deserializer(node.id),
+			node.superClass && deserializer(node.superClass)
+		);
+	}
+	protected id: Identifier;
+	declareVariable(stack: Stack, scopeType: ScopeType, propertyValue?: any) {
+		stack.declareVariable(scopeType, this.id.getName(), propertyValue);
+	}
+}
+
+@Deserializer('ClassExpression')
+export class ClassExpression extends Class {
+	static fromJSON(node: ClassExpression, deserializer: NodeDeserializer<any>): ClassExpression {
+		return new ClassExpression(
+			deserializer(node.body),
+			node.id && deserializer(node.id),
+			node.superClass && deserializer(node.superClass)
+		);
+	}
+}
+
