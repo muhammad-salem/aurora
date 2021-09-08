25dc0b15a6d96767055a5122d82d185650b78508
salemebo <salem.ebo@gmail.com>
2021-09-08 20:14:11 +0200

    add '.js' ext

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,17 +3,17 @@ import { HTMLComponent } from '@ibyar/aurora';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@popperjs/core';
-export * from './app-root/app-root-component';
+export * from './app-root/app-root-component.js';
 
-export * from './person-app/person';
-export * from './person-app/person-app';
+export * from './person-app/person.js';
+export * from './person-app/person-app.js';
 
-export * from './two-way/binding-2-way';
+export * from './two-way/binding-2-way.js';
 
-export * from './video-player/video';
+export * from './video-player/video.js';
 
-import { AppRoot } from './app-root/app-root-component';
-import './pipe-app/pipe-test';
+import { AppRoot } from './app-root/app-root-component.js';
+import './pipe-app/pipe-test.js';
 
 const appRoot = document.getElementById('app-root') as HTMLComponent<AppRoot>;
 appRoot._model.setAppSelector([
