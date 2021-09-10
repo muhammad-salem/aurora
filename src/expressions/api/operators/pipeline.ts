fa63c65e239a665388d90af31ea3bf1706321f14
salemebo <salem.ebo@gmail.com>
2021-09-10 09:52:54 +0200

    impl BindPipelineExpression

--- src/expressions/api/operators/pipeline.ts
+++ src/expressions/api/operators/pipeline.ts
@@ -10,6 +10,7 @@ import { SpreadElement } from '../computing/spread.js';
  *  param |> func:arg1:arg2:arg3
  *  param |> func:arg1:?:arg3
  *  param |> func:arg1:?:arg3:?:arg5
+ *  param |> func:arg1:...?:arg3
  * 
  *  param |> func(arg1, arg2, arg3)
  *  param |> func(arg1, ?, arg3)
@@ -21,11 +22,13 @@ export class PipelineExpression extends AbstractExpressionNode {
 		return new PipelineExpression(
 			deserializer(node.left),
 			deserializer(node.right),
-			node.params.map(param => typeof param === 'string' ? param : deserializer(param))
+			node.arguments.map(arg => typeof arg === 'string' ? arg : deserializer(arg))
 		);
 	}
-	constructor(private left: ExpressionNode, private right: ExpressionNode, private params: (ExpressionNode | '?' | '...?')[] = []) {
+	private arguments: (ExpressionNode | '?' | '...?')[];
+	constructor(private left: ExpressionNode, private right: ExpressionNode, params: (ExpressionNode | '?' | '...?')[] = []) {
 		super();
+		this.arguments = params;
 	}
 	getLeft() {
 		return this.left;
@@ -33,19 +36,18 @@ export class PipelineExpression extends AbstractExpressionNode {
 	getRight() {
 		return this.right;
 	}
-	getParams() {
-		return this.params;
+	getArguments() {
+		return this.arguments;
 	}
 	set(stack: Stack, value: any) {
 		throw new Error(`PipelineNode#set() has no implementation.`);
 	}
-	get(stack: Stack, thisContext?: any) {
+	get(stack: Stack) {
 		const paramValue = this.left.get(stack);
-		const funCallBack = this.right.get(stack, thisContext) as Function;
+		const funCallBack = this.right.get(stack) as Function;
 		const parameters: any[] = [];
-		// const parametersStack = stack.emptyStackProviderWith(parameters);
 		let indexed = false;
-		for (const arg of this.params) {
+		for (const arg of this.arguments) {
 			if (arg === '?') {
 				parameters.push(paramValue);
 				indexed = true;
@@ -62,26 +64,94 @@ export class PipelineExpression extends AbstractExpressionNode {
 				}
 			}
 		}
-		if (indexed) {
-			return funCallBack.call(thisContext, ...parameters);
+		if (!indexed) {
+			parameters.unshift(paramValue);
 		}
-		return funCallBack.call(thisContext, paramValue, ...parameters);
+		return funCallBack(parameters);
 	}
 	events(parent?: string): string[] {
 		return [
 			...this.right.events(),
 			...this.left.events(),
-			...(this.params.filter(param => (param !== '?' && param !== '...?')) as ExpressionNode[]).flatMap(param => param.events!())
+			...(this.arguments.filter(arg => (arg !== '?' && arg !== '...?')) as ExpressionNode[]).flatMap(param => param.events!())
 		];
 	}
 	toString() {
-		return `${this.left.toString()} |> ${this.right.toString()}${this.params.flatMap(param => `:${param.toString()}`).join('')}`;
+		return `${this.left.toString()} |> ${this.right.toString()}${this.arguments.flatMap(arg => `:${arg.toString()}`).join('')}`;
 	}
 	toJson(): object {
 		return {
 			left: this.left.toJSON(),
 			right: this.right.toJSON(),
-			params: this.params?.map(param => typeof param === 'string' ? param : param.toJSON())
+			arguments: this.arguments.map(arg => typeof arg === 'string' ? arg : arg.toJSON())
+		};
+	}
+}
+
+
+/**
+ * pipeline (':|>') operator support syntax:
+ *  param :|> func 					==>	func.call(this=param)
+ *  param |> func:arg1:arg2:arg3	==>	func.call(param, arg1, arg2, arg3)
+ * 
+ *  param |> func(arg1, arg2) ==>	func.call(param, arg1, arg2)
+ */
+@Deserializer('BindPipelineExpression')
+export class BindPipelineExpression extends AbstractExpressionNode {
+	static fromJSON(node: BindPipelineExpression, deserializer: NodeDeserializer): BindPipelineExpression {
+		return new BindPipelineExpression(
+			deserializer(node.left),
+			deserializer(node.right),
+			node.arguments.map(arg => typeof arg === 'string' ? arg : deserializer(arg))
+		);
+	}
+	private arguments: ExpressionNode[];
+	constructor(private left: ExpressionNode, private right: ExpressionNode, params: ExpressionNode[] = []) {
+		super();
+		this.arguments = params;
+	}
+	getLeft() {
+		return this.left;
+	}
+	getRight() {
+		return this.right;
+	}
+	getArguments() {
+		return this.arguments;
+	}
+	set(stack: Stack, value: any) {
+		throw new Error(`BindPipelineExpression#set() has no implementation.`);
+	}
+	get(stack: Stack) {
+		const paramValue = this.left.get(stack);
+		const funCallBack = this.right.get(stack) as Function;
+		const parameters: any[] = [];
+		for (const arg of this.arguments) {
+			if (arg instanceof SpreadElement) {
+				stack.pushBlockScopeFor(parameters);
+				arg.get(stack);
+				stack.popScope();
+			} else {
+				parameters.push(arg.get(stack));
+			}
+		}
+		return funCallBack.apply(paramValue, parameters);
+	}
+	events(parent?: string): string[] {
+		return [
+			...this.right.events(),
+			...this.left.events(),
+			...(this.arguments.flatMap(param => param.events!()))
+		];
+	}
+	toString() {
+		return `${this.left.toString()} :|> ${this.right.toString()}${this.arguments.flatMap(arg => `:${arg.toString()}`).join('')}`;
+	}
+	toJson(): object {
+		return {
+			left: this.left.toJSON(),
+			right: this.right.toJSON(),
+			arguments: this.arguments.map(arg => arg.toJSON())
 		};
 	}
 }
