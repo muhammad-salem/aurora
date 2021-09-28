cf6956ddcd80abb96dbfe9f6412748eb8b50b537
salemebo <salem.ebo@gmail.com>
2021-09-28 02:32:32 +0200

    old impl

--- src/expressions/api/operators/pipeline.ts
+++ src/expressions/api/operators/pipeline.ts
@@ -2,258 +2,161 @@ import type { NodeDeserializer, ExpressionNode } from '../expression.js';
 import type { Stack } from '../../scope/stack.js';
 import { AbstractExpressionNode } from '../abstract.js';
 import { Deserializer } from '../deserialize/deserialize.js';
-import { Identifier } from '../definition/values.js';
-import { CallExpression } from '../computing/call.js';
-import { ArrowFunctionExpression, FunctionExpression } from '../definition/function.js';
-import { NewExpression } from '../computing/new.js';
+import { SpreadElement } from '../computing/spread.js';
 import { MemberExpression } from '../definition/member.js';
-import { SpreadElement } from '../index.js';
-
-export type PipelineOperator = '|>' | ':|>' | '?|>' | '?:|>' | '|>>' | '<|' | '<|:' | '?<|' | '?<|:' | '<<|';
-export type PipelineBodyType = Identifier | CallExpression | NewExpression | FunctionExpression | ArrowFunctionExpression;
-
-export type PipeHandler<T, R> = (argument: T, body: Function, params?: any[], thisArg?: any) => R;
-
-@Deserializer('PipeClause')
-export class PipeClause extends AbstractExpressionNode {
-	static fromJSON(node: PipeClause, deserializer: NodeDeserializer): PipeClause {
-		return new PipeClause(
-			node.operator,
-			deserializer(node.body) as PipelineBodyType
-		);
-	}
-	private static PipeHandler: { [key in PipelineOperator]: PipeHandler<any, any> } = {
-
-		/**
-		 * x |> g  === g(x)
-		 * @param argument 
-		 * @param body 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		'|>': (argument: any, body: Function, params: any[] = [], thisArg?: any) => {
-			params.unshift(argument);
-			return body.apply(thisArg, params);
-		},
-
-		/**
-		 * x :|> g  === g.call(x)
-		 * @param argument 
-		 * @param body 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		':|>': (argument: any, body: Function, params: any[] = [], thisArg?: any) => {
-			return body.apply(argument, params);
-		},
-
-		/***
-		 * x ?|> g  === x && g(x)
-		 */
-		'?|>': (argument: any, body: Function, params: any[] = [], thisArg?: any) => {
-			if (argument == undefined || argument == null) {
-				return argument;
-			}
-			return PipeClause.PipeHandler['|>'](argument, body, params, thisArg);
-		},
-
-		/**
-		 * x ?:|> g  === x && g.call(x)
-		 * @param argument 
-		 * @param body 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		'?:|>': (argument: any, body: Function, params: any[] = [], thisArg?: any) => {
-			if (argument == undefined || argument == null) {
-				return argument;
-			}
-			return PipeClause.PipeHandler[':|>'](argument, body, params, thisArg);
-		},
-
-		/**
-		 * x |> g |>> f === g(f(x))
-		 * @param argument 
-		 * @param body 
-		 * @returns 
-		 */
-		'|>>': (argument: any, body: Function) => {
-			return (x: any) => argument(body(x));
-		},
-
-		/**
-		 * x <| g  === x(g)
-		 * @param body 
-		 * @param argument 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		'<|': (body: Function, argument: any, params: any[] = [], thisArg?: any) => {
-			return PipeClause.PipeHandler['|>'](argument, body, params, thisArg);
-		},
-
-		/**
-		 * x <|: g  === x.call(g)
-		 * @param body 
-		 * @param argument 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		'<|:': (body: Function, argument: any, params: any[] = [], thisArg?: any) => {
-			return PipeClause.PipeHandler[':|>'](argument, body, params, thisArg);
-		},
-
-		/**
-		 * x ?<| g  === x && x(g)
-		 * @param body 
-		 * @param argument 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		'?<|': (body: Function, argument: any, params: any[] = [], thisArg?: any) => {
-			return PipeClause.PipeHandler['?|>'](argument, body, params, thisArg);
-		},
-
-		/**
-		 * x ?<|: g  === x&& x.call(g)
-		 * @param body 
-		 * @param argument 
-		 * @param params 
-		 * @param thisArg 
-		 * @returns 
-		 */
-		'?<|:': (body: Function, argument: any, params: any[] = [], thisArg?: any) => {
-			return PipeClause.PipeHandler['?:|>'](argument, body, params, thisArg);
-		},
-
-		/**
-		 * x |> g <<| f === f(g(x))
-		 * @param body 
-		 * @param argument 
-		 * @returns 
-		 */
-		'<<|': (body: Function, argument: any,) => {
-			return (x: any) => argument(body(x));
-		},
-	};
-	constructor(
-		public operator: PipelineOperator,
-		public body: PipelineBodyType) {
-		super();
-	}
-	set(stack: Stack, value: any) {
-		throw new Error(`PipeClause#set() has no implementation.`);
-	}
-	get(stack: Stack, thisContext?: any) {
-		throw new Error(`PipeClause#get() has no implementation.`);
-	}
-	eval<T, R>(argument: T, stack: Stack): R {
-		if (this.operator.startsWith('?') && !(argument == undefined || argument == null)) {
-			// stop eval the rhs
-			return argument as any;
-		}
-		if (this.body instanceof CallExpression) {
-			return this.evalCallExpression(argument, stack);
-		} else if (this.body instanceof NewExpression) {
-			return this.evalNewExpression(argument, stack);
-		}
-		const funcRef = this.body.get(stack);
-		return PipeClause.PipeHandler[this.operator](argument, funcRef);
-	}
-	private evalNewExpression<T, R>(argument: T, stack: Stack): R {
-		const classExpression = (this.body as NewExpression).getClassName();
-		const classRef = classExpression.get(stack);
-		// new g <| x // is not valid as '(new g)' is valid expression and will be evaluated
-		// x |> new g is valid, will be 'new g(x)'
-		// x ?|> new g is valid, will be 'x && new g(x)'
-		return new classRef(argument);
-	}
-	private evalCallExpression<T, R>(argument: T, stack: Stack): R {
-		const funcExpression = (this.body as CallExpression).getCallee();
-		const parameters = (this.body as CallExpression).getArguments();
-		const params: any[] = [];
-		for (const arg of parameters) {
-			if (arg instanceof SpreadElement) {
-				const paramScope = stack.pushBlockScopeFor(params);
-				arg.get(stack);
-				stack.clearTo(paramScope);
-			} else {
-				params.push(arg.get(stack));
-			}
-		}
-		const funcRef = funcExpression.get(stack);
-		let thisContext: any;
-		if (funcExpression instanceof MemberExpression) {
-			thisContext = funcExpression.getObject().get(stack);
-		}
-		return PipeClause.PipeHandler[this.operator](argument, funcRef, params, thisContext);
-	}
-	events(parent?: string): string[] {
-		return this.body.events();
-	}
-	toString(): string {
-		return `${this.operator} ${this.body.toString()}`;
-	}
-	toJson(key?: string): { [key: string]: any; } {
-		return {
-			operator: this.operator,
-			body: this.body.toJSON(),
-		};
-	}
-}
 
 /**
  * pipeline ('|>') operator support syntax:
  *  param |> func
- *  param |> func:arg2:arg3
- *  param |> func(arg2, arg3)
+ *  param |> func:arg1:arg2:arg3
+ *  param |> func:arg1:?:arg3
+ *  param |> func:arg1:?:arg3:?:arg5
+ *  param |> func:arg1:...?:arg3
+ * 
+ *  param |> func(arg1, arg2, arg3)
+ *  param |> func(arg1, ?, arg3)
+ *  param |> func(arg1, ?, arg3, arg4, ?, arg6)
  */
 @Deserializer('PipelineExpression')
 export class PipelineExpression extends AbstractExpressionNode {
 	static fromJSON(node: PipelineExpression, deserializer: NodeDeserializer): PipelineExpression {
 		return new PipelineExpression(
-			deserializer(node.argument),
-			node.pipes.map(deserializer) as PipeClause[],
+			deserializer(node.left),
+			deserializer(node.right),
+			node.arguments.map(arg => typeof arg === 'string' ? arg : deserializer(arg))
 		);
 	}
-	constructor(private argument: ExpressionNode, private pipes: PipeClause[]) {
+	private arguments: (ExpressionNode | '?' | '...?')[];
+	constructor(private left: ExpressionNode, private right: ExpressionNode, params: (ExpressionNode | '?' | '...?')[] = []) {
 		super();
+		this.arguments = params;
 	}
-	getArgument() {
-		return this.argument;
+	getLeft() {
+		return this.left;
 	}
-	getPipes() {
-		return this.pipes;
+	getRight() {
+		return this.right;
+	}
+	getArguments() {
+		return this.arguments;
 	}
 	set(stack: Stack, value: any) {
-		throw new Error(`PipelineExpression#set() has no implementation.`);
+		throw new Error(`PipelineNode#set() has no implementation.`);
 	}
 	get(stack: Stack) {
-		let result = this.argument.get(stack);
-		for (const pipe of this.pipes) {
-			result = pipe.eval(result, stack);
+		const paramValue = this.left.get(stack);
+		const parameters: any[] = [];
+		let indexed = false;
+		for (const arg of this.arguments) {
+			if (arg === '?') {
+				parameters.push(paramValue);
+				indexed = true;
+			} else if (arg === '...?') {
+				parameters.push(...paramValue);
+				indexed = true;
+			} else {
+				if (arg instanceof SpreadElement) {
+					const paramScope = stack.pushBlockScopeFor(parameters);
+					arg.get(stack);
+					stack.clearTo(paramScope);
+				} else {
+					parameters.push(arg.get(stack));
+				}
+			}
 		}
-		return result;
+		if (!indexed) {
+			parameters.unshift(paramValue);
+		}
+		const funCallBack = this.right.get(stack) as Function;
+		if (this.right instanceof MemberExpression) {
+			const thisArg = this.right.getObject().get(stack);
+			return funCallBack.apply(thisArg, parameters);
+		}
+		return funCallBack(...parameters);
 	}
 	events(parent?: string): string[] {
 		return [
-			...this.argument.events(),
-			...this.pipes.flatMap(pipe => pipe.events()),
+			...this.right.events(),
+			...this.left.events(),
+			...(this.arguments.filter(arg => (arg !== '?' && arg !== '...?')) as ExpressionNode[]).flatMap(param => param.events!())
 		];
 	}
 	toString() {
-		return `${this.argument.toString()} ${this.pipes.map(pipe => pipe.toString()).join(' ')}`;
+		return `${this.left.toString()} |> ${this.right.toString()}${this.arguments.flatMap(arg => `:${arg.toString()}`).join('')}`;
 	}
 	toJson(): object {
 		return {
-			argument: this.argument.toJSON(),
-			right: this.pipes.map(pipe => pipe.toJSON()),
+			left: this.left.toJSON(),
+			right: this.right.toJSON(),
+			arguments: this.arguments.map(arg => typeof arg === 'string' ? arg : arg.toJSON())
+		};
+	}
+}
+
+
+/**
+ * pipeline (':|>') operator support syntax:
+ *  param :|> func 					==>	func.call(this=param)
+ *  param |> func:arg1:arg2:arg3	==>	func.call(param, arg1, arg2, arg3)
+ * 
+ *  param |> func(arg1, arg2) ==>	func.call(param, arg1, arg2)
+ */
+@Deserializer('BindPipelineExpression')
+export class BindPipelineExpression extends AbstractExpressionNode {
+	static fromJSON(node: BindPipelineExpression, deserializer: NodeDeserializer): BindPipelineExpression {
+		return new BindPipelineExpression(
+			deserializer(node.left),
+			deserializer(node.right),
+			node.arguments.map(arg => typeof arg === 'string' ? arg : deserializer(arg))
+		);
+	}
+	private arguments: ExpressionNode[];
+	constructor(private left: ExpressionNode, private right: ExpressionNode, params: ExpressionNode[] = []) {
+		super();
+		this.arguments = params;
+	}
+	getLeft() {
+		return this.left;
+	}
+	getRight() {
+		return this.right;
+	}
+	getArguments() {
+		return this.arguments;
+	}
+	set(stack: Stack, value: any) {
+		throw new Error(`BindPipelineExpression#set() has no implementation.`);
+	}
+	get(stack: Stack) {
+		const paramValue = this.left.get(stack);
+		const funCallBack = this.right.get(stack) as Function;
+		const parameters: any[] = [];
+		for (const arg of this.arguments) {
+			if (arg instanceof SpreadElement) {
+				const paramScope = stack.pushBlockScopeFor(parameters);
+				arg.get(stack);
+				stack.clearTo(paramScope);
+			} else {
+				parameters.push(arg.get(stack));
+			}
+		}
+		return funCallBack.apply(paramValue, parameters);
+	}
+	events(parent?: string): string[] {
+		return [
+			...this.right.events(),
+			...this.left.events(),
+			...(this.arguments.flatMap(param => param.events!()))
+		];
+	}
+	toString() {
+		return `${this.left.toString()} :|> ${this.right.toString()}${this.arguments.flatMap(arg => `:${arg.toString()}`).join('')}`;
+	}
+	toJson(): object {
+		return {
+			left: this.left.toJSON(),
+			right: this.right.toJSON(),
+			arguments: this.arguments.map(arg => arg.toJSON())
 		};
 	}
 }
