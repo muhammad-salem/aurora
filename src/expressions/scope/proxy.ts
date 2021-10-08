84a94aeaab06ea7380397c81f77fa7362848ff4f
salemebo <salem.ebo@gmail.com>
2021-10-08 09:32:32 +0200

    create another proxy for nested objects

--- src/expressions/scope/proxy.ts
+++ src/expressions/scope/proxy.ts
@@ -4,12 +4,25 @@ import type { Scope } from './scope.js';
  * crete new proxy handler object as scoped context
  */
 export class ScopeProxyHandler<T extends object> implements ProxyHandler<T> {
+	private proxyMap = new Map<PropertyKey, T>();
 	constructor(private scope: Scope<T>) { }
 	has(model: T, propertyKey: PropertyKey): boolean {
 		return this.scope.has(propertyKey);
 	}
 	get(model: T, propertyKey: PropertyKey, receiver: any): any {
-		return this.scope.get(propertyKey);
+		if (this.proxyMap.has(propertyKey)) {
+			return this.proxyMap.get(propertyKey);
+		}
+		const value = this.scope.get(propertyKey);
+		if (typeof value === 'object') {
+			const scope = this.scope.getScope(propertyKey);
+			if (scope) {
+				const proxy = new Proxy(value, new ScopeProxyHandler(scope));
+				this.proxyMap.set(propertyKey, proxy);
+				return proxy;
+			}
+		}
+		return value;
 	}
 	set(model: T, propertyKey: PropertyKey, value: any, receiver: any): boolean {
 		return this.scope.set(propertyKey, value);
@@ -18,6 +31,9 @@ export class ScopeProxyHandler<T extends object> implements ProxyHandler<T> {
 		const isDelete = Reflect.deleteProperty(model, propertyKey);
 		if (isDelete) {
 			this.scope.set(propertyKey, undefined);
+			if (this.proxyMap.has(propertyKey)) {
+				this.proxyMap.delete(propertyKey);
+			}
 		}
 		return isDelete;
 	}
