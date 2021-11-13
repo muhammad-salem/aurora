234a29031fc3b8cee3c0dc8db80940d7e4cfaaa3
salemebo <salem.ebo@gmail.com>
2021-11-13 14:20:32 +0200

    update TimeDirective

--- example/src/directive/time.directive.ts
+++ example/src/directive/time.directive.ts
@@ -1,9 +1,7 @@
 import {
-	Directive, DOMParentNode, htmlParser,
-	isModel, OnDestroy, OnInit, ReactiveScope,
-	ScopeSubscription, StructuralDirective, Model, PipeTransform, Pipe
+	Directive, DOMParentNode, OnDestroy, OnInit,
+	StructuralDirective, PipeTransform, Pipe, Component
 } from '@ibyar/aurora';
-import { buildExpressionNodes } from '@ibyar/core/html/expression';
 import { map, Subscription, timer, timestamp } from 'rxjs';
 
 
@@ -18,36 +16,56 @@ export class ToDate implements PipeTransform<number, Date> {
 
 type TimeContext = { time: number, date: number, hh: number, mm: number, ss: number };
 
+const stringLiteralFormat = '`${hh}:${mm}:${ss}`';
+@Component({
+	selector: 'show-time',
+	template: `<div class="alert alert-success" role="alert">
+			<ul>
+				<li>HH:MM:SS {{hh}}:{{mm}}:{{ss}}</li>
+				<li>hh:mm:ss {{${stringLiteralFormat}}} format using regex expression ==> \`$\{hh\}:$\{mm\}:$\{ss\}\`</li>
+				<li>Time: {{time |> toDate}}</li>
+				<li>Data: {{date}}</li>
+			</ul>
+		</div>`
+})
+class ShowTimeComponent implements TimeContext {
+	time: number;
+	date: number;
+	hh: number;
+	mm: number;
+	ss: number;
+}
+
+
 @Directive({
 	selector: '*time',
 })
 export class TimeDirective extends StructuralDirective implements OnInit, OnDestroy {
-
-	private elements: ChildNode[] = [];
-	private fragment: DocumentFragment;
-	private scope: ReactiveScope<TimeContext>;
 	private dateSubscription: Subscription;
-	private scopeSubscription: ScopeSubscription<TimeContext>;
+	private context: TimeContext;
 	onInit(): void {
-		const wrapperNode = ((this.node as DOMParentNode)?.children?.length) ? this.node : this.getDefaultNode();
-		this.fragment = document.createDocumentFragment();
-		const stack = this.directiveStack.copyStack();
-		this.scope = stack.pushBlockReactiveScopeFor({
+		if ((this.templateRef.astNode as DOMParentNode)?.children?.length) {
+			this.initUserView();
+		} else {
+			this.initDefaultView();
+		}
+		this.updateTime();
+	}
+
+	private initUserView() {
+		const initValue = {
 			time: 0,
 			date: 0,
 			hh: 0,
 			mm: 0,
 			ss: 0,
-		});
+		};
+		const viewRef = this.viewContainerRef.createEmbeddedView(this.templateRef, initValue);
+		this.context = viewRef.context;
+	}
 
-		this.render.appendChildToParent(this.fragment, wrapperNode, stack, this.parentNode);
-		const context = this.scope.getContext();
-		if (isModel(context)) {
-			this.scopeSubscription = this.scope.subscribe(this.createScopeHandle(context));
-		}
-		this.fragment.childNodes.forEach(child => this.elements.push(child));
-		this.comment.after(this.fragment);
-		this.updateTime();
+	private initDefaultView() {
+		this.context = this.viewContainerRef.createComponent(ShowTimeComponent);
 	}
 
 	private updateTime() {
@@ -56,47 +74,19 @@ export class TimeDirective extends StructuralDirective implements OnInit, OnDest
 			map(timestamp => timestamp.timestamp),
 			map(timestamp => new Date(timestamp))
 		).subscribe(date => {
-			this.scope.set('hh', date.getHours());
-			this.scope.set('mm', date.getMinutes());
-			this.scope.set('ss', date.getSeconds());
+			this.context.hh = date.getHours();
 
-			this.scope.set('date', date.getDate());
-			this.scope.set('time', date.getTime());
+			this.context.hh = date.getHours();
+			this.context.mm = date.getMinutes();
+			this.context.ss = date.getSeconds();
+
+			this.context.date = date.getDate();
+			this.context.time = date.getTime();
 		});
-	}
-	getDefaultNode() {
-		const stringLiteralFormat = '`${hh}:${mm}:${ss}`';
-		const html = `<div class="alert alert-success" role="alert">
-			<ul>
-				<li>HH:MM:SS {{hh}}:{{mm}}:{{ss}}</li>
-				<li>hh:mm:ss {{${stringLiteralFormat}}} format using regex expression ==> \`$\{hh\}:$\{mm\}:$\{ss\}\`</li>
-				<li>Time: {{time |> toDate}}</li>
-				<li>Data: {{date}}</li>
-			</ul>
-		</div>`;
-		const defaultTemplateNode = htmlParser.toDomRootNode(html);
-		buildExpressionNodes(defaultTemplateNode);
-		return defaultTemplateNode;
-	}
-	private removeOldElements() {
-		if (this.elements.length > 0) {
-			for (const elm of this.elements) {
-				elm.remove();
-			}
-			this.elements.splice(0);
-		}
-	}
-	private createScopeHandle(context: Model) {
-		return (propertyName: keyof TimeContext, oldValue: any, newValue: any) => {
-			if (newValue != oldValue) {
-				context.emitChangeModel(propertyName, []);
-			}
-		};
 	}
 
 	onDestroy() {
 		this.dateSubscription.unsubscribe();
-		this.scopeSubscription.unsubscribe();
-		this.removeOldElements();
+		this.viewContainerRef.clear();
 	}
 }
