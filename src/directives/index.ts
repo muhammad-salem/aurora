3677ad036cb6bd4dc025fcb878e76d9b8773d2ea
salemebo <salem.ebo@gmail.com>
2021-11-18 00:59:07 +0200

    edit export

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,7 +1,12 @@
-export * from './structural/if.js';
-export * from './structural/for.js';
-export * from './structural/while.js';
-export * from './structural/switch.js';
-
+/**
+ * attribute directives
+ */
 export * from './attribute/styles.js';
 export * from './attribute/class.js';
+
+/**
+ * structural directives
+ */
+export * from './structural/for.js';
+export * from './structural/if.js';
+export * from './structural/switch.js';
