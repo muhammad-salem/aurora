7d0f6971266618d939da8a768aa1e7f7ff3c4453
salemebo <salem.ebo@gmail.com>
2021-11-20 12:39:23 +0200

    update events

--- src/expressions/scope/event.ts
+++ src/expressions/scope/event.ts
@@ -2,42 +2,44 @@ import { ExpressionEventMap } from '../api/expression.js';
 import { ReactiveScope, Scope, ScopeContext } from './scope.js';
 import { Stack } from './stack.js';
 
-function visitInnerScope(events: ExpressionEventMap, scope: Scope<ScopeContext>, scopeMap: Map<string, Scope<object>>) {
-	const eventNames = Object.keys(events);
-	if (!eventNames.length) {
-		return;
-	}
+function visitInnerScope(eventNames: string[], events: ExpressionEventMap, scope: Scope<ScopeContext>, scopeMap: Map<string, Scope<object>>) {
 	eventNames.forEach(eventName => {
 		if (eventName.startsWith(':')) {
 			return;
 		}
 		scopeMap.set(eventName, scope);
-		const innerScope = scope.getScope<ScopeContext>(eventName);
-		innerScope && visitInnerScope(events[eventName], innerScope, scopeMap);
+		const nextEvents = Object.keys(events[eventName]);
+		if (!nextEvents.length) {
+			return;
+		}
+		const innerScope = scope.getScopeOrCreat(eventName);
+		visitInnerScope(nextEvents, events[eventName], innerScope, scopeMap);
 	});
 }
 
-export function findScopeByEventMap(events: ExpressionEventMap, stack: Stack): Map<string, Scope<object>> {
+export function findScopeByEventMap(events: ExpressionEventMap, stack: Stack): Map<string, Scope<ScopeContext>> {
 	const scopeMap = new Map<string, Scope<object>>();
 	const rootEventNames = Object.keys(events);
 	rootEventNames.forEach(eventName => {
 		const scope = stack.findScope<ScopeContext>(eventName);
 		scopeMap.set(eventName, scope);
-		const eventScope = scope.getScope<ScopeContext>(eventName);
-		if (eventScope) {
-			visitInnerScope(events[eventName], eventScope, scopeMap);
+		const nextEvents = Object.keys(events[eventName]);
+		if (!nextEvents.length) {
+			return;
 		}
+		const eventScope = scope.getScopeOrCreat<ScopeContext>(eventName);
+		visitInnerScope(nextEvents, events[eventName], eventScope, scopeMap);
 	});
 	return scopeMap;
 }
 
-export function findReactiveScopeByEventMap(events: ExpressionEventMap, stack: Stack): Map<string, ReactiveScope<object>> {
+export function findReactiveScopeByEventMap(events: ExpressionEventMap, stack: Stack): Map<string, ReactiveScope<ScopeContext>> {
 	const allScopes = findScopeByEventMap(events, stack);
 
-	const scopeMap = new Map<string, ReactiveScope<object>>();
+	const scopeMap = new Map<string, ReactiveScope<ScopeContext>>();
 	allScopes.forEach((scope, eventName) => {
 		if (scope instanceof ReactiveScope) {
-			scopeMap.set(eventName, scope as ReactiveScope<object>);
+			scopeMap.set(eventName, scope);
 		}
 	});
 	return scopeMap;
