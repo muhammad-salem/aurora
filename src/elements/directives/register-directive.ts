c46f648ed3b9a49389578c92750a266b10c8764b
salemebo <salem.ebo@gmail.com>
2021-11-22 07:32:45 +0200

    update DirectiveNodeOptions

--- src/elements/directives/register-directive.ts
+++ src/elements/directives/register-directive.ts
@@ -3,10 +3,62 @@
 export interface DirectiveNodeOptions {
 
 	/**
-	 * list of attributes name 
-	 * consists of inputs and outputs and normal attributes
+	 * list of inputs view name
 	 */
-	attributes?: string[];
+	inputs?: string[];
+
+	/**
+	 * list of outputs view name
+	 */
+	outputs?: string[];
+}
+
+class DirectiveNodeInfo {
+	constructor(private inputs?: string[], private outputs?: string[]) { }
+
+	hasAttributes(): boolean {
+		return this.hasInputs() || this.hasOutputs();
+	}
+
+	hasInputs(): boolean {
+		return (this.inputs?.length ?? 0) > 0;
+	}
+
+	hasOutputs(): boolean {
+		return (this.outputs?.length ?? 0) > 0;
+	}
+
+	getAttributes(): string[] | undefined {
+		if (this.inputs && this.outputs) {
+			return this.inputs.concat(this.outputs);
+		} else if (this.inputs) {
+			return this.inputs;
+		} else if (this.outputs) {
+			return this.outputs;
+		}
+		return undefined;
+	}
+
+	getInputs(): string[] | undefined {
+		return this.inputs;
+	}
+
+	getOutputs(): string[] | undefined {
+		return this.outputs;
+	}
+
+	hasAttribute(attributeName: string): boolean {
+		return this.hasInput(attributeName) || this.hasOutput(attributeName);
+	}
+
+	hasInput(inputName: string): boolean {
+		return this.inputs?.includes(inputName) || false;
+	}
+
+	hasOutput(outputName: string): boolean {
+		return this.outputs?.includes(outputName) || false;
+	}
+
 }
 
 export class DirectiveRegistry {
@@ -14,30 +66,38 @@ export class DirectiveRegistry {
 	/**
 	 * store options info about directives
 	 */
-	private directives = new Map<string, DirectiveNodeOptions>();
+	private directives = new Map<string, DirectiveNodeInfo>();
 
 	/**
 	 * register a directive with a name,
-	 * if the directive name exists, will not replace the old directive options
+	 * 
+	 * the directive could be a structural directive or an attribute directive.
+	 * 
+	 * if the directive name exists, will not replace the old directive options.
 	 * @param directiveName 
 	 * @param options contain the attributes of the registered directive name
 	 * @override
 	 */
 	register(directiveName: string, options?: DirectiveNodeOptions): void {
 		if (!this.directives.has(directiveName)) {
-			this.directives.set(directiveName, options ?? {});
+			const info = new DirectiveNodeInfo(options?.inputs, options?.outputs);
+			this.directives.set(directiveName, info);
 		}
 	}
 
 	/**
 	 * replace the current options with a new one.
+	 * 
+	 * the directive could be a structural directive or an attribute directive.
+	 * 
 	 * if the directive name not exists, no set options will be done
 	 * @param directiveName 
 	 * @param options to be replaced
 	 */
 	replace(directiveName: string, options: DirectiveNodeOptions): void {
 		if (this.directives.has(directiveName)) {
-			this.directives.set(directiveName, options);
+			const info = new DirectiveNodeInfo(options?.inputs, options?.outputs);
+			this.directives.set(directiveName, info);
 		}
 	}
 
@@ -46,8 +106,8 @@ export class DirectiveRegistry {
 	 * @param directiveName 
 	 * @returns `boolean`
 	 */
-	has(directiveName: string): boolean {
-		return this.directives.has(directiveName);
+	has(attributeName: string): boolean {
+		return this.directives.has(attributeName);
 	}
 
 	/**
@@ -55,7 +115,7 @@ export class DirectiveRegistry {
 	 * @param directiveName 
 	 * @returns `DirectiveOptions` if the name has been registered, otherwise `undefined`
 	 */
-	get(directiveName: string): DirectiveNodeOptions | undefined {
+	get(directiveName: string): DirectiveNodeInfo | undefined {
 		return this.directives.get(directiveName);
 	}
 
@@ -65,16 +125,76 @@ export class DirectiveRegistry {
 	 * @returns `boolean`
 	 */
 	hasAttributes(directiveName: string): boolean {
-		return (this.directives.get(directiveName)?.attributes?.length ?? 0) > 0;
+		return this.hasInputs(directiveName) || this.hasOutputs(directiveName);
+	}
+
+	hasInputs(directiveName: string): boolean {
+		return this.directives.get(directiveName)?.hasInputs() || false;
+	}
+
+	hasOutputs(directiveName: string): boolean {
+		return this.directives.get(directiveName)?.hasOutputs() || false;
 	}
 
 	/**
-	 * get the value of the registered attributes with a directive
+	 * get the value of the registered inputs and outputs by directive name
 	 * @param directiveName 
 	 * @returns array of strings if found, otherwise `undefined`
 	 */
 	getAttributes(directiveName: string): string[] | undefined {
-		return this.directives.get(directiveName)?.attributes;
+		return this.directives.get(directiveName)?.getAttributes();
+	}
+
+	/**
+	 * get the value of the registered inputs by directive name
+	 * @param directiveName 
+	 * @returns array of strings if found, otherwise `undefined`
+	 */
+	getInputs(directiveName: string): string[] | undefined {
+		return this.directives.get(directiveName)?.getInputs();
+	}
+
+	/**
+	 * get the value of the registered outputs by directive name
+	 * @param directiveName 
+	 * @returns array of strings if found, otherwise `undefined`
+	 */
+	getOutputs(directiveName: string): string[] | undefined {
+		return this.directives.get(directiveName)?.getOutputs();
+	}
+
+	/**
+	 * check if a directive has a attribute
+	 * @param directiveName 
+	 * @param attributeName 
+	 * @returns 
+	 */
+	hasAttribute(directiveName: string, attributeName: string): boolean {
+		return this.directives.get(directiveName)?.hasAttribute(attributeName) || false;
+	}
+
+	/**
+	 * check if has input
+	 * @param directiveName 
+	 * @param inputName 
+	 * @returns 
+	 */
+	hasInput(directiveName: string, inputName: string): boolean {
+		return this.directives.get(directiveName)?.hasInput(inputName) || false;
+	}
+
+	/**
+	 * check if has output
+	 * @param directiveName 
+	 * @param outputName 
+	 * @returns 
+	 */
+	hasOutput(directiveName: string, outputName: string): boolean {
+		return this.directives.get(directiveName)?.hasOutput(outputName) || false;
+	}
+
+	filterDirectives(attributes: string[]): string[] {
+		return attributes.filter(name => this.has(name));
 	}
 
 }
