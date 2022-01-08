c3144ced3eb6d09511b4d05d5085d3d721b37f1d
salemebo <salem.ebo@gmail.com>
2022-01-08 16:09:31 +0200

    disaple override 'get' method for now

--- src/core/component/provider.ts
+++ src/core/component/provider.ts
@@ -1,6 +1,5 @@
 import { createProxyForContext, FunctionProxyHandler, ReactiveScopeControl, ScopeType } from '@ibyar/expressions';
 
-
 export class ElementModelReactiveScope<T extends { [key: PropertyKey]: any }> extends ReactiveScopeControl<T> {
 	static for<T extends object>(context: T, type: ScopeType) {
 		return new ElementModelReactiveScope(context, type);
@@ -20,21 +19,21 @@ export class ElementModelReactiveScope<T extends { [key: PropertyKey]: any }> ex
 	static globalScopeFor<T extends object>(context: T) {
 		return new ElementModelReactiveScope(context, 'global');
 	}
-	private functionProxyMap = new Map<PropertyKey, Function>();
 	private contextProxy = createProxyForContext(this);
+	// private functionProxyMap = new Map<PropertyKey, Function>();
 	getContextProxy() {
 		return this.contextProxy;
 	}
-	get(propertyKey: PropertyKey): any {
-		if (this.functionProxyMap.has(propertyKey)) {
-			return this.functionProxyMap.get(propertyKey);
-		}
-		const value = Reflect.get(this.context, propertyKey);
-		if (typeof value === 'function') {
-			const proxy = new Proxy<Function>(value, new FunctionProxyHandler<Function>(this.contextProxy));
-			this.functionProxyMap.set(propertyKey, proxy);
-			return proxy;
-		}
-		return value;
-	}
+	// get(propertyKey: PropertyKey): any {
+	// 	// if (this.functionProxyMap.has(propertyKey)) {
+	// 	// 	return this.functionProxyMap.get(propertyKey);
+	// 	// }
+	// 	const value = Reflect.get(this.context, propertyKey);
+	// 	// if (typeof value === 'function' && !('constructor' in value)) {
+	// 	// 	const proxy = new Proxy<Function>(value, new FunctionProxyHandler<Function>(this.contextProxy));
+	// 	// 	this.functionProxyMap.set(propertyKey, proxy);
+	// 	// 	return proxy;
+	// 	// }
+	// 	return value;
+	// }
 }
