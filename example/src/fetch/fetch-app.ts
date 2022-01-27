f5adee14a33f2d6327aee27488ce7b9efe742908
salemebo <salem.ebo@gmail.com>
2022-01-27 12:09:06 +0200

    edit fetch example

--- example/src/fetch/fetch-app.ts
+++ example/src/fetch/fetch-app.ts
@@ -3,17 +3,48 @@ import { Component, Input, OnInit } from '@ibyar/aurora';
 
 @Component({
 	selector: 'fetch-app',
-	template: `	<ul class="list-group">
-					<li  *for="let item of list" class="list-group-item">{{item}}</li>
-				</ul>
-				`
+	template: `	<div class="row gx-5">
+		<div class="col">
+			<ul class="list-group">
+				<li *for="let item of list" class="list-group-item" [class]="{'active': selected === item}" @click="selected = item">
+					{{item}}
+				</li>
+			</ul>
+		</div>
+		<div class="col">
+			<button type="button" class="btn btn-link" @click="move(list.indexOf(selected), -1)">UP</button>
+			<button type="button" class="btn btn-link" @click="move(list.indexOf(selected), +1)">Down</button>
+			<button type="button" class="btn btn-link" @click="list.sort()">SORT</button>
+			<button type="button" class="btn btn-link" @click="delete(list.indexOf(selected))">DELETE</button>
+			<button type="button" class="btn btn-link" @click="appendItem()">APPEND</button>
+		</div>
+	</div>`
 })
 export class FetchApp implements OnInit {
-	list?: string[] = undefined;
+	list: number[] = [];
+	selected: number = 1;
 
 	onInit(): void {
 		fetch('/web_modules/@ibyar/example/dist/fetch/data.json')
 			.then(response => response.json())
-			.then((list: string[]) => this.list = list);
+			.then((list: string[]) => this.list = list.map(i => +i));
+	}
+
+	move(index: number, direction: number) {
+		if (!this.list) {
+			return;
+		}
+		const item = this.list.splice(index, 1)[0];
+		if (!item) {
+			return;
+		}
+		this.list.splice(index + direction, 0, item);
+	}
+	delete(index: number) {
+		this.selected = this.list.at(index - 1) ?? 0;
+		return this.list.splice(index, 1)[0];
+	}
+	appendItem() {
+		this.list.push(this.list.length > 0 ? Math.max.apply(Math, this.list) + 1 : 0);
 	}
 }
