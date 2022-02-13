b1064f9dfe1b4f670214fa3f2f38a2047a40ae5a
salemebo <salem.ebo@gmail.com>
2022-02-13 03:39:08 +0200

    edit example

--- example/src/directive/track-by-example.ts
+++ example/src/directive/track-by-example.ts
@@ -10,8 +10,32 @@ export type Hero = {
 @Component({
 	selector: 'track-by-component',
 	template: `
-		<div *for="const hero of heros; trackBy trackByMethod">
-			{{hero.id}} - {{hero.title}}
+		<div class="row">
+			<div class="col-10">
+				<div class="row p-2">
+					*for="const hero of heros; trackBy trackById; let i = index;"
+					<div *for="const hero of heros; trackBy trackById; let i = index;">
+						#{{i}} - {{hero.id}} - {{hero.title}}
+					</div>
+				</div>
+				<div class="row p-2">
+					*for="const hero of heros; trackBy: trackByTitle; let i = index;"
+					<div *for="const hero of heros; trackBy: trackByTitle; let i = index;">
+						#{{i}} - {{hero.id}} - {{hero.title}}
+					</div>
+				</div>
+				<div class="row p-2">
+					*for="const hero of heros; trackBy = (index, heroRef) => heroRef.id; let i = index;"
+					<div *for="const hero of heros; trackBy = (index, heroRef) => heroRef.id; let i = index;">
+						#{{i}} - {{hero.id}} - {{hero.title}}
+					</div>
+				</div>
+			</div>
+			<div class="col-2">
+				<div class="btn-group-vertical">
+					<button type="button" class="btn btn-outline-primary" @click="changeIds()">Change IDS</button>
+				</div>
+			</div>
 		</div>
 	`
 })
@@ -39,8 +63,22 @@ export class TrackByComponent implements OnInit {
 
 	}
 
-	trackByMethod(index: number, hero: Hero): number {
+	trackById(index: number, hero: Hero): number {
 		return hero.id;
 	}
+	trackByTitle(index: number, hero: Hero): string {
+		return hero.title;
+	}
+
+	changeIds() {
+		const heros = this.heros.map(h => Object.assign({}, h));
+		heros.forEach((h, i) => h.id += this.getRandomInt(0, 3));
+		this.heros = heros;
+	}
+	private getRandomInt(min: number, max: number) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
 
 }
