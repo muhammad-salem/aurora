95e3eb1a09deb974c09d9cce6605bed6da672eac
salemebo <salem.ebo@gmail.com>
2022-03-10 07:17:17 +0200

    update element model scope

--- src/core/component/provider.ts
+++ src/core/component/provider.ts
@@ -1,23 +1,23 @@
 import { createProxyForContext, ReactiveScopeControl, ScopeType } from '@ibyar/expressions';
 
 export class ElementModelReactiveScope<T extends { [key: PropertyKey]: any }> extends ReactiveScopeControl<T> {
-	static for<T extends object>(context: T, type: ScopeType) {
-		return new ElementModelReactiveScope(context, type);
+	static for<T extends object>(context: T, type: ScopeType, propertyKeys?: (keyof T)[]) {
+		return new ElementModelReactiveScope(context, type, propertyKeys);
 	}
-	static blockScopeFor<T extends object>(context: T) {
-		return new ElementModelReactiveScope(context, 'block');
+	static blockScopeFor<T extends object>(context: T, propertyKeys?: (keyof T)[]) {
+		return new ElementModelReactiveScope(context, 'block', propertyKeys);
 	}
-	static functionScopeFor<T extends object>(context: T) {
-		return new ElementModelReactiveScope(context, 'function');
+	static functionScopeFor<T extends object>(context: T, propertyKeys?: (keyof T)[]) {
+		return new ElementModelReactiveScope(context, 'function', propertyKeys);
 	}
-	static classScopeFor<T extends object>(context: T) {
-		return new ElementModelReactiveScope(context, 'class');
+	static classScopeFor<T extends object>(context: T, propertyKeys?: (keyof T)[]) {
+		return new ElementModelReactiveScope(context, 'class', propertyKeys);
 	}
-	static moduleScopeFor<T extends object>(context: T) {
-		return new ElementModelReactiveScope(context, 'module');
+	static moduleScopeFor<T extends object>(context: T, propertyKeys?: (keyof T)[]) {
+		return new ElementModelReactiveScope(context, 'module', propertyKeys);
 	}
-	static globalScopeFor<T extends object>(context: T) {
-		return new ElementModelReactiveScope(context, 'global');
+	static globalScopeFor<T extends object>(context: T, propertyKeys?: (keyof T)[]) {
+		return new ElementModelReactiveScope(context, 'global', propertyKeys);
 	}
 	private contextProxy = createProxyForContext(this);
 	getContextProxy(): T {
