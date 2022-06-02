7f610ab3f14862dc9ddbc5a6f43f5106633c1905
salemebo <salem.ebo@gmail.com>
2022-06-02 07:46:17 +0200

    edit example

--- test/expressions/class.spec.mjs
+++ test/expressions/class.spec.mjs
@@ -2,7 +2,35 @@ import { JavaScriptAppParser } from '@ibyar/expressions';
 
 const source = `
 
-class Book {}
+class Book {
+	static #PRIVATE_STATIC_FIELD;
+	name = 'A Song of Ice and Fire';
+	#privateField;
+
+	getName() {
+		return this.name;
+	}
+}
+
+// class ClassWithPrivateField {
+//   #privateField;
+// }
+
+// class ClassWithPrivateMethod {
+//   #privateMethod() {
+//     return 'hello world';
+//   }
+// }
+
+// class ClassWithPrivateStaticField {
+//   static #PRIVATE_STATIC_FIELD;
+// }
+
+// class ClassWithPrivateStaticMethod {
+//   static #privateStaticMethod() {
+//     return 'hello world';
+//   }
+// }
 
 `;
 
