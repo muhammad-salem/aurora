5e9b67b6c671fec00a68e66755ff76ae80eb0294
salemebo <salem.ebo@gmail.com>
2022-06-03 21:01:59 +0200

    add isDeclarationExpression type guard

--- src/expressions/api/utils.ts
+++ src/expressions/api/utils.ts
@@ -1,3 +1,10 @@
+import { DeclarationExpression, ExpressionNode } from './expression.js';
+
 export interface TypeOf<T> {
 	new(...params: any[]): T;
+}
+
+
+export function isDeclarationExpression(node: ExpressionNode): node is DeclarationExpression {
+	return typeof (node as any).declareVariable === 'function';
 }
