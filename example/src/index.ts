70043ac23797eae760b8f65ec7d2e5e045e8497c
salemebo <salem.ebo@gmail.com>
2022-06-03 22:56:50 +0200

    lazy loading

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,11 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@popperjs/core';
 
-// import structural directives first
-// so it can register itself with the html parser as a node
-export * from './directive/add-note.directive.js';
-export * from './directive/notify-user.directive.js';
-export * from './directive/time.directive.js';
 
 export * from './route/component-outlet.js';
 export * from './route/router-outlet.js';
