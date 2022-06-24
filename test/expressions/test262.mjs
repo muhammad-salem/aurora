1451a7f4bcac3f14e6abd9d346d90788aaf5ff40
salemebo <salem.ebo@gmail.com>
2022-06-25 00:15:58 +0200

    update passExcludes

--- test/expressions/test262.mjs
+++ test/expressions/test262.mjs
@@ -11,37 +11,26 @@ function parse(src, { isModule, earlyErrors }) {
 
 
 let passExcludes = [
-	'110fa1efdd0868b8.js', '1270d541e0fd6af8.js', '14199f22a45c7e30.js',
-	'17302b9b0cab0c69.module.js', '18cc9a6b7038070f.js', '1a7800a74a866638.js',
-	'1d3dd296a717e478.js', '29e41f46ede71f11.js', '2bd8ae9202baa31e.js',
-	'2dc1c08d0bff6eba.js', '31ad88cae27258b7.js', '332f0bc46d28db25.js',
-	'3990bb94b19b1071.js', '3ae4f46daa688c58.js', '3b36d546985cd9cb.js',
+	'1a7800a74a866638.js', '29e41f46ede71f11.js', '2aa1db78027ba395.js',
+	'31ad88cae27258b7.js', '3990bb94b19b1071.js', '3ae4f46daa688c58.js',
 	'3d2ab39608730a47.js', '3e48826018d23c85.js', '4672c2ef688237c9.js',
-	'4ae32442eef8a4e0.js', '4f5419fe648c691b.js', '4fee4ac53bdfd7f7.js',
+	'4789c3375f112cd4.js', '4f5419fe648c691b.js', '4fee4ac53bdfd7f7.js',
 	'54e70df597a4f9a3.js', '569a2c1bad3beeb2.js', '56fd564979894636.js',
-	'585b857c11763bad.js', '5a2a8e992fa4fe37.js', '5b39aca97d9006f4.js',
-	'5beffd72ddb47f13.js', '5d5b9de6d9b95f3e.js', '5dd65055dace49bc.js',
-	'6196b3f969486455.js', '641ac9060a206183.js', '65401ed8dc152370.js',
-	'6d8728cbc7bfe6b5.js', '71e066a0fa01825b.js', '77a541b0502d0bde.js',
-	'77db52b103913973.js', '7b514406528ff126.js', '7c03e5eb6a9f6f1a.js',
-	'84f901eb37273117.js', '8c80f7ee04352eba.js', '8e3f0660b32fbfd2.js',
-	'8e609bb71c20b858.module.js', '8e6c915d1746636d.js', '8ec6a55806087669.js',
-	'925443c6cf79aa88.js', '92a997b1ba17876e.js', '946bee37652a31fa.js',
-	'94cb828d5dcfd136.js', '9aa93e1e417ce8e3.js', '9ec644dbf797e95c.js',
-	'9f0d8eb6f7ab8180.js', '9fe1d41db318afba.js', 'a022debc42a58f0c.js',
-	'a62c6323a3696fa8.js', 'ac6bbe8465f70ebd.module.js', 'affd557fd820e1f2.js',
-	'afffb6d317e53b92.js', 'b0c6752e1db068ed.js', 'b15ab152f8531a9f.js',
-	'b376d3924d77aa8a.js', 'b5cf21a87ec272d1.js', 'ba00173ff473e7da.js',
-	'bb41f0778f00f131.js', 'be9d538d5041fd5f.js', 'c412905e229d6f2b.js',
-	'c532e126a986c1d4.js', 'c83a2dcf75fa419a.js', 'c8b9a4d186ec2eb8.js',
-	'ca7a0ca0d22f30f8.js', 'ccd1f89a0344e04e.js', 'ce0aaec02d5d4465.js',
-	'd1eafbc6bda219a7.js', 'd3ac25ddc7ba9779.js', 'd79a08ea5cc1e2f6.js',
-	'd97144839fbdca91.js', 'da3756d1f8acb3c5.js', 'dc3afa2f13259ae0.js',
-	'dec1ae80150e1664.js', 'e03ae54743348d7d.js', 'e08e181172bad2b1.js',
-	'e2470430b235b9bb.module.js', 'e4bd395227b4ee8e.js', 'e5570b178254bfb9.js',
-	'eed97872dd924560.js', 'f0d9a7a2f5d42210.js', 'f1bf02f18fa71ba7.js',
-	'f8d843a30c73377a.js', 'fa5b398eeef697a6.js', 'fa736f4b0cf19c0c.js',
-	'fbacebe72fb15fed.module.js', 'fbcd793ec7c82779.js', 'fe24fc72de1ef7cc.js',
+	'598a5cedba92154d.js', '5a2a8e992fa4fe37.js', '5b39aca97d9006f4.js',
+	'5beffd72ddb47f13.js', '5d5b9de6d9b95f3e.js', '6196b3f969486455.js',
+	'714be6d28082eaa7.js', '71e066a0fa01825b.js', '77db52b103913973.js',
+	'7b514406528ff126.js', '7c03e5eb6a9f6f1a.js', '84f901eb37273117.js',
+	'8e3f0660b32fbfd2.js', '8ec6a55806087669.js', '9027dae72a91a9ed.js',
+	'925443c6cf79aa88.js', '946bee37652a31fa.js', '94cb828d5dcfd136.js',
+	'96941f16c2d7cec4.js', '9aa93e1e417ce8e3.js', '9ec644dbf797e95c.js',
+	'9f0d8eb6f7ab8180.js', '9fe1d41db318afba.js', 'ad06370e34811a6a.js',
+	'affd557fd820e1f2.js', 'afffb6d317e53b92.js', 'b376d3924d77aa8a.js',
+	'b5cf21a87ec272d1.js', 'ba00173ff473e7da.js', 'bb41f0778f00f131.js',
+	'c412905e229d6f2b.js', 'c532e126a986c1d4.js', 'c83a2dcf75fa419a.js',
+	'c8b9a4d186ec2eb8.js', 'ce0aaec02d5d4465.js', 'd79a08ea5cc1e2f6.js',
+	'dc3afa2f13259ae0.js', 'dec1ae80150e1664.js', 'e03ae54743348d7d.js',
+	'e08e181172bad2b1.js', 'e5570b178254bfb9.js', 'eed97872dd924560.js',
+	'fa5b398eeef697a6.js', 'fa736f4b0cf19c0c.js',
 ];
 
 let failExcludes = [];
@@ -53,8 +42,9 @@ const errors = [];
 const rootTest = './node_modules/test262-parser-tests';
 let x = 0;
 readdirSync(`${rootTest}/pass`)
-	.filter(f => !passExcludes.includes(f))
+	// .filter(f => passExcludes.includes(f))
 	.forEach(f => {
+		// f = '1a7800a74a866638.js';
 		console.log('parse `pass` file: ', f, ++x);
 		let firstTree, secondTree;
 		try {
@@ -73,10 +63,25 @@ readdirSync(`${rootTest}/pass`)
 			deepStrictEqual(firstTree, secondTree);
 		} catch (error) {
 			errors.push(f);
+			// throw error;
 		}
 	});
 
-const errorString = errors.map(JSON.stringify).map(s => s.replace(/"/g, "'")).map((s, i) => ((i + 1) % 3) ? s + ',' : `${s},\n`).join('');
+const errorString = errors
+	.sort((a, b) => {
+		if (a.endsWith('.module.js') && b.endsWith('.module.js')) {
+			return a.localeCompare(b);
+		} else if (a.endsWith('.module.js')) {
+			return -1;
+		} if (b.endsWith('.module.js')) {
+			return 1;
+		} else {
+			return a.localeCompare(b);
+		}
+	})
+	.map(JSON.stringify)
+	.map(s => s.replace(/"/g, "'"))
+	.map((s, i) => ((i + 1) % 3) ? s + ',' : `${s},\n`).join('');
 console.log('pass errors', errors.length, `[\n${errorString}]`);
 
 x = 0;
