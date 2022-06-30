3a390d5e765164b7e63bb0f3356da89a0353de3f
salemebo <salem.ebo@gmail.com>
2022-06-30 10:43:41 +0200

    update passExcludes, all pass

--- test/expressions/test262.mjs
+++ test/expressions/test262.mjs
@@ -9,23 +9,18 @@ function parse(src, { isModule, earlyErrors }) {
 }
 
 
-let passExcludes = [
-	'29e41f46ede71f11.js', '6196b3f969486455.js', '925443c6cf79aa88.js',
-	'c8b9a4d186ec2eb8.js', 'e5570b178254bfb9.js',
-];
-
+let passExcludes = [];
 let failExcludes = [];
-let earlyExcludes = ['557.script.js', '558.script.js', '559.script.js', '560.script.js', '561.script.js', '563.script.js', '564.script.js', '565.script.js', '566.script.js', '567.script.js', '568.script.js', '569.script.js', '570.script.js', '571.script.js', '572.script.js', '574.script.js', '575.script.js', '576.script.js', '577.script.js', '578.script.js', '579.script.js', '580.script.js', '581.script.js', '582.script.js', '583.script.js', '585.script.js', '586.script.js', '587.script.js', '588.script.js', '589.script.js', '590.script.js', '591.script.js', '592.script.js', '593.script.js', '594.script.js', '596.script.js', '597.script.js', '598.script.js', '599.script.js', '600.script.js', '601.script.js', '602.script.js'];
-earlyExcludes = [];
+let earlyExcludes = [];
 
 const errors = [];
 
 const rootTest = './node_modules/test262-parser-tests';
 let x = 0;
 readdirSync(`${rootTest}/pass`)
-	// .filter(f => passExcludes.includes(f))
+	.filter(f => !passExcludes.includes(f))
 	.forEach(f => {
-		// f = 'b376d3924d77aa8a.js';
+		// f = 'ce5f3bc27d5ccaac.js';
 		console.log('parse `pass` file: ', f, ++x);
 		let firstTree, secondTree;
 		try {
@@ -54,51 +49,75 @@ readdirSync(`${rootTest}/pass`)
 		}
 	});
 
-const errorString = errors
-	.sort((a, b) => {
-		if (a.endsWith('.module.js') && b.endsWith('.module.js')) {
-			return a.localeCompare(b);
-		} else if (a.endsWith('.module.js')) {
-			return -1;
-		} if (b.endsWith('.module.js')) {
-			return 1;
-		} else {
-			return a.localeCompare(b);
-		}
-	})
-	.map(JSON.stringify)
-	.map(s => s.replace(/"/g, "'"))
-	.map((s, i) => ((i + 1) % 3) ? s + ',' : `${s},\n`).join('');
-console.log('pass errors', errors.length, `[\n${errorString}]`);
+printErrors(errors);
+errors.splice(0);
 
 x = 0;
 readdirSync(`${rootTest}/fail`)
 	.filter(f => !failExcludes.includes(f))
 	.forEach(f => {
 		console.log('parse `fail` file: ', f, ++x);
-		throws(() => {
-			parse(
-				readFileSync(`${rootTest}/fail/${f}`, 'utf8'),
-				{ isModule: f.includes('.module.js'), earlyErrors: false }
-			);
-		});
+		try {
+			throws(() => {
+				parse(
+					readFileSync(`${rootTest}/fail/${f}`, 'utf8'),
+					{ isModule: f.includes('.module.js'), earlyErrors: false }
+				);
+			});
+		} catch (error) {
+			// console.error('error', f, error);
+			console.error('error', f, readFileSync(`${rootTest}/fail/${f}`, 'utf8'));
+			errors.push(f);
+			// throw error;
+		}
 	});
 
+printErrors(errors);
+errors.splice(0);
 x = 0;
 readdirSync(`${rootTest}/early`)
 	.filter(f => !earlyExcludes.includes(f))
 	.forEach(f => {
 		console.log('parse `early` file: ', f, ++x);
-		doesNotThrow(() => {
-			parse(
-				readFileSync(`${rootTest}/early/${f}`, 'utf8'),
-				{ isModule: f.includes('.module.js'), earlyErrors: false }
-			);
-		});
-		throws(() => {
-			parse(
-				readFileSync(`${rootTest}/early/${f}`, 'utf8'),
-				{ isModule: f.includes('.module.js'), earlyErrors: true }
-			);
-		});
+		try {
+			doesNotThrow(() => {
+				parse(
+					readFileSync(`${rootTest}/early/${f}`, 'utf8'),
+					{ isModule: f.includes('.module.js'), earlyErrors: false }
+				);
+			});
+			throws(() => {
+				parse(
+					readFileSync(`${rootTest}/early/${f}`, 'utf8'),
+					{ isModule: f.includes('.module.js'), earlyErrors: true }
+				);
+			});
+		} catch (error) {
+			// console.error('error', f, error);
+			console.error('error', f, readFileSync(`${rootTest}/early/${f}`, 'utf8'));
+			errors.push(f);
+			// throw error;
+		}
+
 	});
+
+printErrors(errors);
+
+function printErrors(errors) {
+	const errorString = errors
+		.sort((a, b) => {
+			if (a.endsWith('.module.js') && b.endsWith('.module.js')) {
+				return a.localeCompare(b);
+			} else if (a.endsWith('.module.js')) {
+				return -1;
+			} if (b.endsWith('.module.js')) {
+				return 1;
+			} else {
+				return a.localeCompare(b);
+			}
+		})
+		.map(JSON.stringify)
+		.map(s => s.replace(/"/g, "'"))
+		.map((s, i) => ((i + 1) % 3) ? s + ',' : `${s},\n`).join('');
+	console.log('pass errors', errors.length, `[\n${errorString}]`);
+}
