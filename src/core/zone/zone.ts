8fec7b968506e2284bd02b6c3582e7b7140e2361
salemebo <salem.ebo@gmail.com>
2022-07-09 23:46:59 +0200

    update zone

--- src/core/zone/zone.ts
+++ src/core/zone/zone.ts
@@ -44,9 +44,54 @@ export class AuroraZone implements AuroraZone {
 		if ((Zone as any)['TaskTrackingZoneSpec']) {
 			self._inner = self._inner.fork(new ((Zone as any)['TaskTrackingZoneSpec'] as any));
 		}
+		const ref: { clone: () => void, detectChanges: () => void, }[] = [];
 		self._inner = self._inner.fork({
 			name: 'aurora',
 			properties: { 'aurora-zone': true },
+			onScheduleTask: (parentZoneDelegate, currentZone, targetZone, task) => {
+				console.log('onScheduleTask', targetZone.name, task.callback.toString().split('\n').slice(0, 2));
+				if (task instanceof ScopeTask) {
+					ref.push(task.scope)
+				}
+				else if (task instanceof StackTask) {
+					ref.push(task.stack);
+				} else {
+					const last = ref.at(-1);
+					const invoke = task.invoke;
+
+					task.invoke = function (...args: any[]) {
+						try {
+							last?.clone();
+							invoke.apply(this, args);
+						} finally {
+							console.log('detectChanges', last);
+							last?.detectChanges();
+						}
+					};
+				}
+				return parentZoneDelegate.scheduleTask(targetZone, task);
+			},
+			// onHasTask: (parentZoneDelegate, currentZone, targetZone, hasTaskState) => {
+			// 	console.log('onHasTask', parentZoneDelegate.zone.name, currentZone.name, targetZone.name, hasTaskState);
+			// },
+			// onInvokeTask: (parentZoneDelegate, currentZone, targetZone, task, applyThis, applyArgs?) => {
+			// 	console.log('onInvokeTask', task.callback.toString().split('\n').slice(0, 2));
+			// 	return parentZoneDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
+			// },
+			onCancelTask: (parentZoneDelegate, currentZone, targetZone, task) => {
+				console.log('onCancelTask', targetZone.name, task.callback.toString().split('\n').slice(0, 2));
+				let index: number = -1;
+				if (task instanceof ScopeTask) {
+					index = ref.indexOf(task.scope);
+				}
+				else if (task instanceof StackTask) {
+					index = ref.indexOf(task.stack);
+				}
+				if (index !== -1) {
+					ref.splice(index, 1);
+				}
+				return parentZoneDelegate.cancelTask(targetZone, task);
+			},
 		});
 	}
 
