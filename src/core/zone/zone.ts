8de4adc8ce50778a93445405ffc6573a5aa82c0f
salemebo <salem.ebo@gmail.com>
2022-07-10 18:55:37 +0200

    try catch and finally

--- src/core/zone/zone.ts
+++ src/core/zone/zone.ts
@@ -1,20 +1,29 @@
 /// <reference types='zone.js' />
-import { ReactiveScopeControl, ScopeContext, Stack } from '@ibyar/expressions';
-import { ScopeTask, StackTask } from './task.js';
+import { EventEmitter } from '../component/events.js';
 
 const noop = () => { };
 const EMPTY_PAYLOAD = {};
 
 export interface AuroraZone {
+
+	readonly onTry: EventEmitter<void>;
+	readonly onCatch: EventEmitter<void>;
+	readonly onFinal: EventEmitter<void>;
+
 	run<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined): T;
 	runTask<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined, name?: string | undefined): T;
-	runScopeTask<T>(scope: ReactiveScopeControl<any>, callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T;
-	runStackTask<T>(stack: Stack, callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T;
 	runGuarded<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined): T;
 	runOutsideAurora<T>(callback: (...args: any[]) => T): T;
 }
 
-export class AuroraZone implements AuroraZone {
+
+abstract class AbstractAuroraZone {
+	readonly onTry: EventEmitter<void> = new EventEmitter<void>();
+	readonly onCatch: EventEmitter<void> = new EventEmitter<void>();
+	readonly onFinal: EventEmitter<void> = new EventEmitter<void>();
+}
+
+export class AuroraZone extends AbstractAuroraZone implements AuroraZone {
 	static isInAuroraZone(): boolean {
 		return typeof Zone !== 'undefined' && Zone.current.get('aurora-zone') === true;
 	}
@@ -31,66 +40,40 @@ export class AuroraZone implements AuroraZone {
 		}
 	}
 
-
 	constructor() {
 		if (typeof Zone == 'undefined') {
 			throw new Error(`In this configuration Zone.js is  required`);
 		}
 		Zone.assertZonePatched();
+		super();
 		const self = this as any as AuroraZonePrivate;
-
 		self._outer = self._inner = Zone.current;
-
 		if ((Zone as any)['TaskTrackingZoneSpec']) {
 			self._inner = self._inner.fork(new ((Zone as any)['TaskTrackingZoneSpec'] as any));
 		}
-		const ref: { clone: () => void, detectChanges: () => void, }[] = [];
 		self._inner = self._inner.fork({
 			name: 'aurora',
 			properties: { 'aurora-zone': true },
-			onScheduleTask: (parentZoneDelegate, currentZone, targetZone, task) => {
-				console.log('onScheduleTask', targetZone.name, task.callback.toString().split('\n').slice(0, 2));
-				if (task instanceof ScopeTask) {
-					ref.push(task.scope)
+			onInvoke: (parentZoneDelegate, currentZone, targetZone, delegate, applyThis, applyArgs?, source?) => {
+				try {
+					this.onTry.emit();
+					return parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
+				} finally {
+					this.onFinal.emit();
 				}
-				else if (task instanceof StackTask) {
-					ref.push(task.stack);
-				} else {
-					const last = ref.at(-1);
-					const invoke = task.invoke;
-
-					task.invoke = function (...args: any[]) {
-						try {
-							last?.clone();
-							invoke.apply(this, args);
-						} finally {
-							console.log('detectChanges', last);
-							last?.detectChanges();
-						}
-					};
-				}
-				return parentZoneDelegate.scheduleTask(targetZone, task);
 			},
-			// onHasTask: (parentZoneDelegate, currentZone, targetZone, hasTaskState) => {
-			// 	console.log('onHasTask', parentZoneDelegate.zone.name, currentZone.name, targetZone.name, hasTaskState);
-			// },
-			// onInvokeTask: (parentZoneDelegate, currentZone, targetZone, task, applyThis, applyArgs?) => {
-			// 	console.log('onInvokeTask', task.callback.toString().split('\n').slice(0, 2));
-			// 	return parentZoneDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
-			// },
-			onCancelTask: (parentZoneDelegate, currentZone, targetZone, task) => {
-				console.log('onCancelTask', targetZone.name, task.callback.toString().split('\n').slice(0, 2));
-				let index: number = -1;
-				if (task instanceof ScopeTask) {
-					index = ref.indexOf(task.scope);
+			onInvokeTask: (parentZoneDelegate, currentZone, targetZone, task, applyThis, applyArgs?) => {
+				try {
+					this.onTry.emit();
+					return parentZoneDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
+				} finally {
+					this.onFinal.emit();
 				}
-				else if (task instanceof StackTask) {
-					index = ref.indexOf(task.stack);
-				}
-				if (index !== -1) {
-					ref.splice(index, 1);
-				}
-				return parentZoneDelegate.cancelTask(targetZone, task);
+			},
+			onHandleError: (parentZoneDelegate, currentZone, targetZone, error) => {
+				parentZoneDelegate.handleError(targetZone, error);
+				self.runOutsideAurora(() => self.onCatch.emit(error));
+				return false;
 			},
 		});
 	}
@@ -101,27 +84,7 @@ export class AuroraZone implements AuroraZone {
 
 	runTask<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T {
 		const zone = (this as any as AuroraZonePrivate)._inner;
-		const task = zone.scheduleEventTask('AuroraZoneEvent: ' + name, callback, EMPTY_PAYLOAD, noop, noop);
-		try {
-			return zone.runTask(task, applyThis, applyArgs);
-		} finally {
-			zone.cancelTask(task);
-		}
-	}
-
-	runScopeTask<T>(scope: ReactiveScopeControl<any>, callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T {
-		const zone = (this as any as AuroraZonePrivate)._inner;
-		const task = zone.scheduleTask(ScopeTask.eventTask(scope, 'AuroraZoneEvent: ' + name, callback, EMPTY_PAYLOAD, noop, noop))
-		try {
-			return zone.runTask(task, applyThis, applyArgs);
-		} finally {
-			zone.cancelTask(task);
-		}
-	}
-
-	runStackTask<T>(stack: Stack, callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T {
-		const zone = (this as any as AuroraZonePrivate)._inner;
-		const task = zone.scheduleTask(StackTask.eventTask(stack, 'AuroraZoneEvent: ' + name, callback, EMPTY_PAYLOAD, noop, noop))
+		const task = zone.scheduleEventTask('AuroraZoneEvent: ' + name ?? '', callback, EMPTY_PAYLOAD, noop, noop);
 		try {
 			return zone.runTask(task, applyThis, applyArgs);
 		} finally {
@@ -145,32 +108,31 @@ interface AuroraZonePrivate extends AuroraZone {
 }
 
 
-export class NoopAuroraZone implements AuroraZone {
+export class NoopAuroraZone extends AbstractAuroraZone implements AuroraZone {
+
+	readonly onTry: EventEmitter<void> = new EventEmitter<void>();
+	readonly onFinal: EventEmitter<void> = new EventEmitter<void>();
+	readonly onCatch: EventEmitter<void> = new EventEmitter<void>();
+
+	private runCallback<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined): T {
+		try {
+			this.onTry.emit();
+			return callback.apply(applyThis, applyArgs!);
+		} catch (error) {
+			this.onCatch.emit();
+			throw error;
+		} finally {
+			this.onFinal.emit();
+		}
+	}
 	run<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined): T {
-		return callback.apply(applyThis, applyArgs!);
+		return this.runCallback(callback, applyThis, applyArgs);
 	}
 	runTask<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined, name?: string | undefined): T {
-		return callback.apply(applyThis, applyArgs!);
-	}
-	runScopeTask<T>(scope: ReactiveScopeControl<ScopeContext>, callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T {
-		try {
-			scope.detach()
-			return callback.apply(applyThis, applyArgs!);
-		} finally {
-			scope.reattach();
-		}
-	}
-
-	runStackTask<T>(stack: Stack, callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[], name?: string): T {
-		try {
-			stack.detach()
-			return callback.apply(applyThis, applyArgs!);
-		} finally {
-			stack.reattach();
-		}
+		return this.runCallback(callback, applyThis, applyArgs);
 	}
 	runGuarded<T>(callback: (...args: any[]) => T, applyThis?: any, applyArgs?: any[] | undefined): T {
-		return callback.apply(applyThis, applyArgs!);
+		return this.runCallback(callback, applyThis, applyArgs);
 	}
 	runOutsideAurora<T>(callback: (...args: any[]) => T): T {
 		return callback();
