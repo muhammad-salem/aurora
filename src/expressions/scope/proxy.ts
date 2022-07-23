d30a03f3e7c1ab6381a8516fe651aaae1b16c1fc
salemebo <salem.ebo@gmail.com>
2022-07-23 15:13:37 +0200

    remove comments

--- src/expressions/scope/proxy.ts
+++ src/expressions/scope/proxy.ts
@@ -1,28 +1,5 @@
 import type { Scope, Context } from './scope.js';
 
-// export class FunctionProxyHandler<T extends Function> implements ProxyHandler<T> {
-// 	constructor(private thisContext: object) { }
-// 	apply(targetFunc: T, targetThisArg: any, argArray: any[]): any {
-// 		return targetFunc.apply(this.computeThisArg(targetThisArg), argArray);
-// 	}
-
-// 	private computeThisArg(targetThisArg: any): any {
-// 		switch (true) {
-// 			// exclude classes that have binding hook
-// 			case this.thisContext instanceof Map:
-// 			case this.thisContext instanceof Set:
-// 			case this.thisContext instanceof Date:
-// 			case this.thisContext instanceof WeakMap:
-// 			case this.thisContext instanceof WeakSet:
-// 			case this.thisContext instanceof Promise:
-// 			case HTMLElement && this.thisContext instanceof HTMLElement:
-// 				return this.thisContext;
-// 			default:
-// 				return targetThisArg;
-// 		}
-// 	}
-// }
-
 /**
  * crete new proxy handler object as scoped context
  */
@@ -42,22 +19,6 @@ export class ScopeProxyHandler<T extends Context> implements ProxyHandler<Contex
 			return value;
 		}
 		return createProxyForContext(scope);
-
-		// const value = this.scope.get(propertyKey);
-		// if (value == null) {
-		// 	return value;
-		// }
-		// if (typeof value === 'object') {
-		// 	const scope = this.scope.getScope(propertyKey);
-		// 	if (scope) {
-		// 		return new Proxy(value, new ScopeProxyHandler(scope));
-		// 	}
-		// } else if (typeof value === 'function') {
-		// 	return new Proxy(value, this.functionHandler
-		// 		?? (this.functionHandler = new FunctionProxyHandler(this.scope.getContext()))
-		// 	);
-		// }
-		// return value;
 	}
 	set(model: T, propertyKey: PropertyKey, value: any, receiver: any): boolean {
 		return this.scope.set(propertyKey, value);
