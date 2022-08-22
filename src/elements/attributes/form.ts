b16f53caedba632a65a7c19d04de8fda76ac24cf
salemebo <salem.ebo@gmail.com>
2022-08-22 20:59:19 +0200

    update form

--- src/elements/attributes/form.ts
+++ src/elements/attributes/form.ts
@@ -1,4 +1,46 @@
 /**
+ * https://html.spec.whatwg.org/multipage/forms.html
+ */
+
+import { isValidCustomElementName } from './tags.js';
+
+
+/**
+ * check the formAssociated property on the associated constructor. 
+ * @param tagName tag name of the defined custom element
+ * @returns boolean
+ */
+export function isFormAssociatedCustomElementByTag(tagName: string) {
+	return !!(customElements.get(tagName) as (CustomElementConstructor & { formAssociated: boolean }) | undefined)?.formAssociated;
+}
+
+/**
+ * check the formAssociated property on the associated constructor. 
+ * @param constructor the associated constructor
+ * @returns boolean
+ */
+export function isFormAssociatedCustomElementByConstructor(constructor: CustomElementConstructor) {
+	return !!(constructor as (CustomElementConstructor & { formAssociated: boolean }) | undefined)?.formAssociated;
+}
+
+/**
+ * check the formAssociated property on the associated constructor on already created element. 
+ * @param el the element
+ * @returns boolean
+ */
+export function isFormAssociatedCustomElementByElement(el: HTMLElement) {
+	return el?.constructor && isFormAssociatedCustomElementByConstructor(el.constructor as CustomElementConstructor);
+}
+
+export function isFormAssociatedCustomElement(item: string | CustomElementConstructor | HTMLElement) {
+	return typeof item === 'string'
+		? isFormAssociatedCustomElementByTag(item)
+		: typeof item === 'function'
+			? isFormAssociatedCustomElementByConstructor(item)
+			: isFormAssociatedCustomElementByElement(item);
+}
+
+/**
  *  form-associated elements
  * A number of the elements are form-associated elements, which means they can have a form owner.
  * @param tagName 
@@ -18,9 +60,38 @@ export function isFormElement(tagName: string): boolean {
 		case 'progress':
 		case 'select':
 		case 'textarea':
-			return true;
-		default: return false;
+		default:
+			break;
 	}
+	if (isValidCustomElementName(tagName)) {
+		return isFormAssociatedCustomElementByTag(tagName);
+	}
+	return false;
+}
+
+/**
+ * Denotes elements that are listed in the form.elements and fieldset.elements APIs.
+ * These elements also have a form content attribute, and a matching form IDL attribute,
+ * that allow authors to specify an explicit form owner.
+ * @param tagName 
+ */
+export function isListedFormElement(tagName: string): boolean {
+	switch (tagName) {
+		case 'button':
+		case 'fieldset':
+		case 'input':
+		case 'object':
+		case 'output':
+		case 'select':
+		case 'textarea':
+			return true;
+		default:
+			break;
+	}
+	if (isValidCustomElementName(tagName)) {
+		return isFormAssociatedCustomElementByTag(tagName);
+	}
+	return false;
 }
 
 /**
@@ -36,13 +107,20 @@ export function isSubmittableElement(tagName: string): boolean {
 		case 'select':
 		case 'textarea':
 			return true;
-		default: return false;
+		default:
+			break;
 	}
+	if (isValidCustomElementName(tagName)) {
+		return isFormAssociatedCustomElementByTag(tagName);
+	}
+	return false;
 }
 
 /**
  * Resettable elements
+ * Denotes elements that can be affected when a form element is reset.
  * @param tagName 
+ * @returns 
  */
 export function isResettableElement(tagName: string): boolean {
 	switch (tagName) {
@@ -51,8 +129,13 @@ export function isResettableElement(tagName: string): boolean {
 		case 'select':
 		case 'textarea':
 			return true;
-		default: return false;
+		default:
+			break;
 	}
+	if (isValidCustomElementName(tagName)) {
+		return isFormAssociatedCustomElementByTag(tagName);
+	}
+	return false;
 }
 
 
@@ -75,11 +158,11 @@ export function isAutocapitalizeInheritingElement(tagName: string): boolean {
 }
 
 /**
- * Labelable Elements
- * Some elements, not all of them form-associated, are categorized as labelable elements.
+ * Label-able Elements
+ * Some elements, not all of them form-associated, are categorized as label-able elements.
  * These are elements that can be associated with a label element.
  * 
- * this function ignore the 'type' attribute of 'input' tage as '<input type="hidden" />' is consdered as not labled element 
+ * this function ignore the 'type' attribute of 'input' tag as '<input type="hidden" />' is considered as not labeled element 
  * @param tagName the tested tag name
  */
 export function isFormLabelableElement(tagName: string): boolean {
@@ -92,6 +175,11 @@ export function isFormLabelableElement(tagName: string): boolean {
 		case 'select':
 		case 'textarea':
 			return true;
-		default: return false;
+		default:
+			break;
 	}
+	if (isValidCustomElementName(tagName)) {
+		return isFormAssociatedCustomElementByTag(tagName);
+	}
+	return false;
 }
