6792109f978ac9734fbe93cc67b3bf461fe5b219
salemebo <salem.ebo@gmail.com>
2022-08-22 23:25:53 +0200

    implements FormAssociatedCustomElement

--- src/core/view/form-view.ts
+++ src/core/view/form-view.ts
@@ -1,82 +1,73 @@
 import type { TypeOf } from '../utils/typeof.js';
 import { ComponentRef } from '../component/component.js';
-import { CustomFormElement, HTMLFormElement, HTMLFormElementComponent } from '../component/custom-element.js';
+import {
+	FormAssociatedCustomElement, HTMLFormElement,
+	FormAssociatedComponent,
+	ValueControl,
+	isValueControl,
+} from '../component/custom-element.js';
 import { baseFactoryView } from './base-view.js';
 
-export function baseFormFactoryView<T extends Object>(htmlElementType: TypeOf<HTMLElement>): TypeOf<HTMLFormElementComponent<T>> {
-	const baseViewClass = baseFactoryView<T>(htmlElementType);
-	class BaseFormView<T> extends baseViewClass implements CustomFormElement {
+export const NOOP_CONTROL_CHANGE = () => { };
 
-		// Identify the element as a form-associated custom element
+
+export function baseFormFactoryView<T extends Object>(htmlElementType: TypeOf<HTMLElement>): TypeOf<FormAssociatedComponent<T>> {
+	const baseViewClass = baseFactoryView<T>(htmlElementType);
+	class BaseFormView<T> extends baseViewClass implements FormAssociatedCustomElement {
+
+		/**
+		 * Identify the element as a form-associated custom element
+		 */
 		static formAssociated = true;
 
-		_disabled: boolean;
-		_required: boolean;
-		_value: any;
+		private _internals: ElementInternals;
+		private _form: HTMLFormElement | null;
+		private _valueControl?: ValueControl<T>;
 
-		// private internals_: ElementInternals;
 		constructor(componentRef: ComponentRef<T>, modelClass: TypeOf<T>) {
 			super(componentRef, modelClass);
-			// this.internals_ = this.attachInternals();
+			this._internals = this.attachInternals();
+			let valueControl: ValueControl<any> | undefined;
+			if (typeof componentRef.formAssociated === 'function') {
+				if (this._model instanceof componentRef.formAssociated) {
+					valueControl == this._model;
+				} else {
+					const args: any[] = []; /* resolve dependency injection*/;
+					valueControl = new componentRef.formAssociated(args);
+				}
+			} else if (componentRef.formAssociated === true && isValueControl(this._model)) {
+				valueControl == this._model;
+			}
+			valueControl && this.registerValueControl(valueControl);
+		}
+		get internals(): ElementInternals {
+			return this._internals;
+		}
+		get form(): HTMLFormElement | null {
+			return this._form;
+		}
+		get valueControl(): ValueControl<T> | undefined {
+			return this._valueControl;
+		}
+		registerValueControl(valueControl: ValueControl): void {
+			if (this.valueControl) {
+				this.valueControl.registerOnChange(NOOP_CONTROL_CHANGE);
+			}
+			this._valueControl = valueControl;
+			this._valueControl?.registerOnChange((value: any) => this.internals.setFormValue(value));
 		}
 
-		formAssociatedCallback(form: HTMLFormElement): void {
-			throw new Error('Method not implemented.');
+		formAssociatedCallback(form: HTMLFormElement | null): void {
+			this._form = form;
 		}
 		formDisabledCallback(disabled: boolean): void {
-			throw new Error('Method not implemented.');
+			this._valueControl?.setDisabledState?.(disabled);
 		}
 		formResetCallback(): void {
-			throw new Error('Method not implemented.');
+			this._valueControl?.writeValue({ mode: 'reset' });
 		}
 		formStateRestoreCallback(value: any, mode: 'restore' | 'autocomplete'): void {
-			throw new Error('Method not implemented.');
-		}
-
-		// get form() { return this.internals_.form; }
-		// get name() { return this.getAttribute('name'); }
-		// get type() { return this.localName; }
-		// get validity() { return this.internals_.validity; }
-		// get validationMessage() { return this.internals_.validationMessage; }
-		// get willValidate() { return this.internals_.willValidate; }
-
-		// checkValidity() { return this.internals_.checkValidity(); }
-		// reportValidity() { return this.internals_.reportValidity(); }
-
-		set disabled(disabled) {
-			this._disabled = disabled;
-			this.toggleAttribute('disabled', disabled);
-		}
-
-		get disabled() {
-			return this._disabled;
-		}
-
-		set required(required) {
-			this._required = required;
-			this.toggleAttribute('required', required);
-			// if (!this.value) {
-			//     this.internals.setValidity({
-			//         valueMissing: true
-			//     }, 'This field is required');
-			// } else {
-			//     this.internals.setValidity({
-			//         valueMissing: false
-			//     });
-			// }
-		}
-
-		get required() {
-			return this._required;
-		}
-
-		set value(value: any) {
-			this._value = value;
-			// this.internals.setFormValue(value);
-		}
-
-		get value() {
-			return this._value;
+			this._valueControl?.writeValue({ value, mode });
 		}
 	}
 	return BaseFormView;
