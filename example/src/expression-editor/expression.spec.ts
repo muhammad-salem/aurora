3538f6555516b992a39e54ff4e716fefb8636323
salemebo <salem.ebo@gmail.com>
2022-09-02 18:26:40 +0200

    update class example

--- example/src/expression-editor/expression.spec.ts
+++ example/src/expression-editor/expression.spec.ts
@@ -20,39 +20,72 @@ console.log(y.i, y.getJ());
 
 export const CLASS_EXAMPLE = `
 class ClassWithPublicField {
-  publicField;
+	publicField = 9;
 }
+console.log(new ClassWithPublicField().publicField);
+
 
 class ClassWithPublicMethod {
-  publicMethod () { }
+	publicMethod() { return 8; }
 }
+console.log(new ClassWithPublicMethod().publicMethod());
+
 
 class ClassWithPrivateField {
-  #privateField;
+	#privateField = 99;
+
+	getPrivate() {
+		return this.#privateField;
+	}
 }
+console.log(new ClassWithPrivateField().getPrivate());
 
 class ClassWithPrivateMethod {
-  #privateMethod() {
-    return 'hello world';
-  }
+	#privateMethod() {
+		return 'hello world';
+	}
+
+	getPrivate() {
+		return this.#privateMethod();
+	}
 }
+
+console.log(new ClassWithPrivateMethod().getPrivate());
 
 class ClassWithPublicStaticField {
-  static PUBLIC_STATIC_FIELD;
+	static PUBLIC_STATIC_FIELD = 77;
 }
+
+console.log(ClassWithPublicStaticField.PUBLIC_STATIC_FIELD);
+
 
 class ClassWithPublicStaticMethod {
-  static publicStaticMethod() {};
+	static publicStaticMethod() { return 88; };
 }
+
+console.log(ClassWithPublicStaticMethod.publicStaticMethod());
+
 
 class ClassWithPrivateStaticField {
-  static #PRIVATE_STATIC_FIELD;
+	static #PRIVATE_STATIC_FIELD = 55;
+
+	static getPrivate() {
+		return this.#PRIVATE_STATIC_FIELD;
+	}
 }
 
+console.log(ClassWithPrivateStaticField.getPrivate());
+
 class ClassWithPrivateStaticMethod {
-  static #privateStaticMethod() {
-    return 'hello world';
-  }
+	static #privateStaticMethod() {
+		return 'hello world';
+	}
+
+	static getPrivate() {
+		return this.#privateStaticMethod();
+	}
 }
+console.log(ClassWithPrivateStaticMethod.getPrivate());
+
 
 `;
