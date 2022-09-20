5951aa0a6d9e9752240bf4d5fa80cee1918ca34f
salemebo <salem.ebo@gmail.com>
2022-09-20 21:09:56 +0200

    add abstract form api

--- src/forms/forms/form-array.ts
+++ src/forms/forms/form-array.ts
@@ -1,14 +1,49 @@
 import { AttributeDirective, Directive } from '@ibyar/core';
 import { AbstractControl } from './form-control.js';
+import { Validator, AsyncValidator } from './validators.js';
 
 
 export abstract class AbstractFormArray<T> extends AbstractControl<T> {
 	controls: AbstractControl<any>[];
-
 }
 
 export class FormArray<T> extends AbstractFormArray<T> {
-
+	get valid(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get invalid(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get pristine(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get dirty(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get touched(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get untouched(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	updateValue(value: T | null): void {
+		throw new Error('Method not implemented.');
+	}
+	addValidator(validator: Validator): void {
+		throw new Error('Method not implemented.');
+	}
+	removeValidator(validator: Validator): void {
+		throw new Error('Method not implemented.');
+	}
+	addAsyncValidator(validator: AsyncValidator): void {
+		throw new Error('Method not implemented.');
+	}
+	removeAsyncValidator(validator: AsyncValidator): void {
+		throw new Error('Method not implemented.');
+	}
+	updateValidity(): void {
+		throw new Error('Method not implemented.');
+	}
 }
 
 @Directive({
--- src/forms/forms/form-control.ts
+++ src/forms/forms/form-control.ts
@@ -6,6 +6,59 @@ export abstract class AbstractControl<T> {
 	validator?: Validator[] | null;
 	asyncValidators?: AsyncValidator[] | null;
 	errors?: { [key: string]: any } | null;
+
+	abstract get valid(): boolean;
+	abstract get invalid(): boolean;
+	abstract get pristine(): boolean;
+	abstract get dirty(): boolean;
+	abstract get touched(): boolean;
+	abstract get untouched(): boolean;
+
+	abstract updateValue(value: T | null): void;
+	abstract addValidator(validator: Validator): void;
+	abstract removeValidator(validator: Validator): void;
+	abstract addAsyncValidator(validator: AsyncValidator): void;
+	abstract removeAsyncValidator(validator: AsyncValidator): void;
+	abstract updateValidity(): void;
+}
+
+export class FormControl<T> extends AbstractControl<T> {
+	get valid(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get invalid(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get pristine(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get dirty(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get touched(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	get untouched(): boolean {
+		throw new Error('Method not implemented.');
+	}
+	updateValue(value: T | null): void {
+		throw new Error('Method not implemented.');
+	}
+	addValidator(validator: Validator): void {
+		throw new Error('Method not implemented.');
+	}
+	removeValidator(validator: Validator): void {
+		throw new Error('Method not implemented.');
+	}
+	addAsyncValidator(validator: AsyncValidator): void {
+		throw new Error('Method not implemented.');
+	}
+	removeAsyncValidator(validator: AsyncValidator): void {
+		throw new Error('Method not implemented.');
+	}
+	updateValidity(): void {
+		throw new Error('Method not implemented.');
+	}
 }
 
 
