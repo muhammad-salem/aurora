56ab2b0b177c307807553436b241717c833a30a0
salemebo <salem.ebo@gmail.com>
2022-09-23 09:36:13 +0200

    fix webpack error `Critical dependency: the request of a dependency is an expression`

--- bundles/webpack/common.config.js
+++ bundles/webpack/common.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
 	entry: './src/index.ts',
 	module: {
+		exprContextCritical: false,
 		rules: [
 			{
 				test: /\.tsx?$/,
