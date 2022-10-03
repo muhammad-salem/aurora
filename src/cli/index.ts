a41e6811e745c3682e6c73f637ec30970fcd8bb2
salemebo <salem.ebo@gmail.com>
2022-10-03 08:30:07 +0200

    remove unused modules

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,8 @@
-export * from './compiler/index.js';
-export * from './factory/index.js';
-export * from './transformer/index.js';
-export * from './visitor/index.js';
+export * from './transformer/factory.js';
+export * from './transformer/before-component.js';
+export * from './transformer/after-declarations-component.js';
+
+
+export * from './compiler/compile.js';
+
+export * from './elements/tags.js';
--- src/cli/transformer/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './factory.js';
-export * from './before-component.js';
-export * from './after-declarations-component.js';
