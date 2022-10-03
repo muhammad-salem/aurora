676e9707110c08fa31c68fd898c21d1532c88376
salemebo <salem.ebo@gmail.com>
2022-10-03 07:46:36 +0200

    remove `dom` related type modules from node exports

--- src/elements/node.ts
+++ src/elements/node.ts
@@ -6,7 +6,3 @@ export * from './ast/dom.js';
 export * from './node/html-parser.js';
 
 export * from './directives/register-directive.js';
-
-export * from './template/template-parser.js';
-
-export * from './accessibility/index.js';
