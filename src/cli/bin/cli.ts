a11096111f6c7e0bb46cb4b9b70c48180cb22ae2
salemebo <salem.ebo@gmail.com>
2022-11-19 15:16:51 +0200

    update cli, show help message

--- src/cli/bin/cli.ts
+++ src/cli/bin/cli.ts
@@ -1,7 +1,54 @@
 #!/usr/bin/env node
 
+import { exit } from 'process';
 
-if (process.argv.includes('-b') || process.argv.includes('--build')) {
+const CLI_VERSION = '0.8.3';
+
+const args = process.argv;
+const inputs = args.slice(2);
+
+const showHelp = inputs.includes('-h') || inputs.includes('--help');
+const printVersion = inputs.includes('-v') || inputs.includes('--version');
+
+const runBuild = inputs.includes('-b') || inputs.includes('--build');
+const generateTypes = inputs.includes('-gt') || inputs.includes('--generate-types');
+
+if (showHelp) {
+	const help =
+		`
+Version ${CLI_VERSION}
+Usage: esmpack [config path] [options]
+
+if no config file in the commend will try to search for file names
+'esmpack.config.js', 'esmpack.config.mjs' and 'esmpack.config.json'.
+
+Examples:
+    ibyar
+    ibyar esmpack.config.js.js
+    ibyar esmpack.config.json -w -d
+    ibyar -v
+    ibyar --help
+
+Options:
+    -b      --build             compile the project source code with ibyar transformers
+    -gt     --generate-types    generate "web-types.json" files, and typescript
+	                            definitions '.d.ts' files. 
+	                            you can import this file later in your "index.ts" 
+								or "polyfills.ts" file, so any editor "VS Code" can
+								support autocomplete easily,
+    -h      --help              print help message
+    -v      --version           output the version number`;
+	console.log(help);
+	exit();
+}
+
+
+if (printVersion) {
+	console.log(CLI_VERSION);
+	exit();
+}
+
+if (runBuild) {
 	import('../compiler/compiler.js').then(module => {
 		if (process.argv.includes('-w') || process.argv.includes('--watch')) {
 			module.compileAndWatchArgs();
@@ -9,4 +56,8 @@ if (process.argv.includes('-b') || process.argv.includes('--build')) {
 			module.compileArgs();
 		}
 	});
+}
+
+if (generateTypes) {
+	console.log('generate types not supported yet.');
 }
