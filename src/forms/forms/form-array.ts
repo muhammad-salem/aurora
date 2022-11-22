0edfdeca2eb81e2fec2fc1f12c3bc76f2bb48899
salemebo <salem.ebo@gmail.com>
2022-11-22 21:58:14 +0200

    update form controls

--- src/forms/forms/form-array.ts
+++ src/forms/forms/form-array.ts
@@ -1,13 +1,20 @@
 import { AttributeDirective, Directive } from '@ibyar/core';
 import { AbstractControl } from './form-control.js';
-import { Validator, AsyncValidator } from './validators.js';
 
 
 export abstract class AbstractFormArray<T> extends AbstractControl<T> {
-	controls: AbstractControl<any>[];
+	constructor(protected controls: AbstractControl<T[keyof T]>[]) {
+		super();
+	}
+	abstract get<C extends AbstractControl<any>>(index: number): C;
 }
 
 export class FormArray<T> extends AbstractFormArray<T> {
+
+	constructor(controls: AbstractControl<T[keyof T]>[]) {
+		super(controls);
+	}
+
 	get valid(): boolean {
 		throw new Error('Method not implemented.');
 	}
@@ -26,19 +33,10 @@ export class FormArray<T> extends AbstractFormArray<T> {
 	get untouched(): boolean {
 		throw new Error('Method not implemented.');
 	}
+	get<C extends AbstractControl<any>>(index: number): C {
+		throw new Error('Method not implemented.');
+	}
 	updateValue(value: T | null): void {
-		throw new Error('Method not implemented.');
-	}
-	addValidator(validator: Validator): void {
-		throw new Error('Method not implemented.');
-	}
-	removeValidator(validator: Validator): void {
-		throw new Error('Method not implemented.');
-	}
-	addAsyncValidator(validator: AsyncValidator): void {
-		throw new Error('Method not implemented.');
-	}
-	removeAsyncValidator(validator: AsyncValidator): void {
 		throw new Error('Method not implemented.');
 	}
 	updateValidity(): void {
--- src/forms/forms/form-group.ts
+++ src/forms/forms/form-group.ts
@@ -1,10 +1,17 @@
-import { AttributeDirective, Directive } from '@ibyar/core';
-import { AbstractControl } from './form-control.js';
-import { Validator, AsyncValidator } from './validators.js';
+import { AttributeDirective, Directive, Input, OnInit } from '@ibyar/core';
+import { ControlValue, isControlValue } from '../builder/form-builder.js';
+import { AbstractControl, FormControl } from './form-control.js';
 
+export abstract class AbstractFormGroup<T extends { [K in keyof T]: AbstractControl<any>; } = any> extends AbstractControl<T> {
+	controls: Record<keyof T, AbstractControl<T[keyof T]>>;
+	abstract get<C extends AbstractControl<any>>(key: string): C;
 
-export abstract class AbstractFormGroup<T> extends AbstractControl<T> {
-	controls: AbstractControl<any>[];
+	abstract addControl<K extends string & keyof T>(name: K, control: T[K]): AbstractControl<T[K]>;
+	abstract addControl<K extends string & keyof T>(name: K, control: ControlValue<T[K]>): AbstractControl<T[K]>;
+	abstract addControl<K extends string & keyof T>(name: K, control: AbstractControl<T[K]>): AbstractControl<T[K]>;
+	abstract addControl<K extends string & keyof T>(name: K, value: T[K] | ControlValue<T[K]> | AbstractControl<T[K]>): AbstractControl<T[K]>;
+
+	abstract removeControl<K extends string & keyof T>(name: K): void;
 }
 
 export class FormGroup<T> extends AbstractFormGroup<T> {
@@ -26,19 +33,26 @@ export class FormGroup<T> extends AbstractFormGroup<T> {
 	get untouched(): boolean {
 		throw new Error('Method not implemented.');
 	}
+	get<C extends AbstractControl<any>>(key: string): C {
+		throw new Error('Method not implemented.');
+	}
+
+	addControl<K extends string & keyof T>(name: K, control: T[K]): AbstractControl<T[K]>;
+	addControl<K extends string & keyof T>(name: K, control: ControlValue<T[K]>): AbstractControl<T[K]>;
+	addControl<K extends string & keyof T>(name: K, control: AbstractControl<T[K]>): AbstractControl<T[K]>;
+	addControl<K extends string & keyof T>(name: K, value: T[K] | ControlValue<T[K]> | AbstractControl<T[K]>): AbstractControl<T[K]> {
+		const control = value instanceof AbstractControl
+			? value
+			: isControlValue(value)
+				? new FormControl(value.value, value.disabled)
+				: new FormControl(value);
+		this.controls[name] = control;
+		return control;
+	}
+	removeControl<K extends string & keyof T>(name: K): void {
+		delete this.controls[name];
+	}
 	updateValue(value: T | null): void {
-		throw new Error('Method not implemented.');
-	}
-	addValidator(validator: Validator): void {
-		throw new Error('Method not implemented.');
-	}
-	removeValidator(validator: Validator): void {
-		throw new Error('Method not implemented.');
-	}
-	addAsyncValidator(validator: AsyncValidator): void {
-		throw new Error('Method not implemented.');
-	}
-	removeAsyncValidator(validator: AsyncValidator): void {
 		throw new Error('Method not implemented.');
 	}
 	updateValidity(): void {
@@ -50,7 +64,16 @@ export class FormGroup<T> extends AbstractFormGroup<T> {
 @Directive({
 	selector: 'formGroup'
 })
-export class FormGroupDirective extends AttributeDirective {
+export class FormGroupDirective extends AttributeDirective implements OnInit {
 
+	declare protected el: HTMLFormElement;
+
+	@Input('formGroup')
+	formGroup: FormGroup<any>;
+
+	onInit(): void {
+		console.log('el', this.el);
+		console.log('formGroup', this.formGroup);
+	}
 
 }
