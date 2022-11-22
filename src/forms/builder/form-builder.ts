093912dc5a56a4c24f75b1a9c0b6655d5bb39d3f
salemebo <salem.ebo@gmail.com>
2022-11-22 21:58:50 +0200

    update form builder

--- src/forms/builder/form-builder.ts
+++ src/forms/builder/form-builder.ts
@@ -1,15 +1,68 @@
+import { FormArray } from '../forms/form-array.js';
+import { AbstractControl, FormControl } from '../forms/form-control.js';
 import { FormGroup } from '../forms/form-group.js';
 import { AsyncValidator, Validator } from '../forms/validators.js';
 
 export type ControlOptions = { validators?: Validator | Validator[], asyncValidators?: AsyncValidator | AsyncValidator[] }
 
+export type ControlValue<T> = { value?: T | null, disabled: boolean };
+
+export function isControlValue<T>(ref: any): ref is ControlValue<T> {
+	return ref.value || ref.disabled;
+}
 
 export class FormBuilder {
 
-	public group<T>(group: Record<string, {}>, options?: ControlOptions): FormGroup<T> {
-		const formGroup = new FormGroup<T>();
+	public control<T>(value?: T | ControlValue<T> | null, options?: ControlOptions): FormControl<T> {
+		const { value: controlValue, disabled } = isControlValue(value)
+			? value
+			: { value, disabled: false };
+		const formControl = new FormControl<T>(controlValue, disabled);
+		this.initValidators(formControl, options);
+		return formControl;
+	}
 
+	public group<T>(group: Record<keyof T, T[keyof T] | ControlValue<T[keyof T]> | FormControl<T[keyof T]>>, options?: ControlOptions): FormGroup<T> {
+		const formGroup = new FormGroup<T>();
+		this.initValidators(formGroup, options);
+		(Object.keys(group) as (keyof T & string)[]).forEach(key => {
+			formGroup.addControl(key, this.createControl(group[key]));
+		});
 		return formGroup;
+	}
+
+	public array<T>(array: AbstractControl<T[keyof T]>[], options?: ControlOptions): FormArray<T> {
+		const controls: AbstractControl<T[keyof T]>[] = array.map(item => this.createControl(item));
+		const formArray = new FormArray<T>(controls);
+		this.initValidators(formArray, options);
+		return formArray;
+	}
+
+	private createControl(item: AbstractControl<any> | Record<string, any> | any) {
+		if (item instanceof AbstractControl) {
+			return item;
+		} else if (typeof item !== 'object') {
+			return this.control(item);
+		} else if (Array.isArray(item)) {
+			return this.array(item);
+		}
+		return this.group(item);
+	}
+
+	private initValidators(control: AbstractControl<any>, options?: ControlOptions) {
+		if (!options) {
+			return;
+		}
+		const validators = this.getSafeArrayValue(options.validators);
+		const asyncValidators = this.getSafeArrayValue(options.asyncValidators);
+		validators.forEach(validator => control.addValidator(validator));
+		asyncValidators.forEach(validator => control.addAsyncValidator(validator));
+	}
+
+	private getSafeArrayValue<T>(object?: T | T[] | null): T[] {
+		return Array.isArray(object)
+			? object
+			: typeof object === 'object' && object !== null ? [object] : [];
 	}
 
 }
