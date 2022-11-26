afd163f3dbcf7985d7e12b6c9db83baae866af30
salemebo <salem.ebo@gmail.com>
2022-11-26 09:23:26 +0200

    add mappers

--- src/forms/forms/mappers.ts
+++ src/forms/forms/mappers.ts
@@ -14,3 +14,15 @@ export class InputDateMapper implements Mapper<string | number, Date> {
 		return new Date(input);
 	}
 }
+
+export class InputDefaultMapper<T = any> implements Mapper<T, T>{
+	convert(input: T): T {
+		return input;
+	}
+}
+
+export class Mappers {
+	static default = new InputDefaultMapper();
+	static number = new InputNumberMapper();
+	static date = new InputDateMapper();
+}
