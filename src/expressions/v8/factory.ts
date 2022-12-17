161c73e9db5ab9fc14afc714a407b024d7bb80de
salemebo <salem.ebo@gmail.com>
2022-12-18 00:50:20 +0200

    add ExpressionNodeFactory

---
+++ src/expressions/v8/factory.ts
@@ -0,0 +1,108 @@
+import type { NodeFactory, SourcePositionFactory } from './node.js';
+import { Super } from '../api/class/class.js';
+import { DebuggerStatement } from '../api/computing/debugger.js';
+import { Literal, ThisExpression } from '../api/definition/values.js';
+import { ExpressionNode, SourceLocation } from '../api/expression.js';
+import { UnaryExpression } from '../api/operators/unary.js';
+import { EmptyStatement } from '../api/statement/control/empty.js';
+import { ExpressionStatement } from '../api/definition/statement.js';
+import { BlockStatement } from '../api/statement/control/block.js';
+import { CatchClauseNode, ThrowStatement, TryCatchNode } from '../api/computing/throw.js';
+
+
+export class ExpressionNodeSourcePosition implements SourcePositionFactory {
+	private newLineRegex = new RegExp('\n', 'g');
+
+	constructor(private source: string) { }
+
+	private getLineNumber(index: number, defaultValue: number) {
+		return this.source.substring(0, index).match(this.newLineRegex)?.length ?? defaultValue;
+	}
+
+	private getColumnNumber(index: number) {
+		const end = this.source.lastIndexOf('\n', index);
+		if (end === -1) {
+			return index;
+		}
+		return index - end - 1;
+	}
+
+	public createSourcePosition(range?: [number, number]): SourceLocation | undefined {
+		if (!range || !this.source) {
+			return;
+		}
+		const startLine = this.getLineNumber(range[0], 1);
+		const endLine = this.getLineNumber(range[1], startLine);
+		const startColumn = this.getColumnNumber(range[0]);
+		const endColumn = this.getColumnNumber(range[1]);
+		return {
+			start: { line: startLine, column: startColumn },
+			end: { line: endLine, column: endColumn },
+		};
+	}
+
+	rangeOfExpressionStatement(list: ExpressionNode[]): [number, number] | undefined {
+		const start = list.at(0)!.range?.[0];
+		const end = list.at(-1)!.range?.[1];
+		const range: [number, number] | undefined = (!!start && !!end) ? [start, end] : undefined;
+		return range;
+	}
+
+}
+
+export class ExpressionNodeFactory implements NodeFactory {
+
+	constructor(private rangeFactory?: SourcePositionFactory) { }
+
+	createDebuggerStatement(range?: [number, number]): DebuggerStatement {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new DebuggerStatement(range, loc);
+	}
+
+	createSuper(range?: [number, number]): Super {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new Super(range, loc);
+	}
+	createThis(range?: [number, number]): ThisExpression {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new ThisExpression(range, loc);
+	}
+	createNull(range?: [number, number]): Literal<null> {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new Literal(null, undefined, undefined, undefined, range, loc);
+	}
+	createTrue(range?: [number, number]): Literal<true> {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new Literal(true, undefined, undefined, undefined, range, loc);
+	}
+	createFalse(range?: [number, number]): Literal<false> {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new Literal(false, undefined, undefined, undefined, range, loc);
+	}
+	createVoidZero(argument: ExpressionNode, range?: [number, number]): UnaryExpression {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new UnaryExpression('void', argument, range, loc);
+	}
+	createEmptyStatement(range?: [number, number]): EmptyStatement {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new EmptyStatement(range, loc);
+	}
+	createExpressionStatement(list: ExpressionNode[]): ExpressionNode {
+		const range = this.rangeFactory?.rangeOfExpressionStatement(list);
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new ExpressionStatement(list, range, loc);
+	}
+	createCatchClause(block: BlockStatement, identifier?: ExpressionNode, range?: [number, number]): CatchClauseNode {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new CatchClauseNode(block, identifier, range, loc);
+	}
+	createTryStatement(tryBlock: BlockStatement, catchBlock?: ExpressionNode, finallyBlock?: ExpressionNode, range?: [number, number]): TryCatchNode {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new TryCatchNode(tryBlock, catchBlock, finallyBlock, range, loc);
+	}
+	createThrowStatement(exception: ExpressionNode, range?: [number, number]): ThrowStatement {
+		const loc = this.rangeFactory?.createSourcePosition(range);
+		return new ThrowStatement(exception, range, loc);
+	}
+
+}
---
+++ src/expressions/v8/node.ts
@@ -0,0 +1,30 @@
+import type { ExpressionNode, SourceLocation } from '../api/expression.js';
+import { Super } from '../api/class/class.js';
+import { DebuggerStatement } from '../api/computing/debugger.js';
+import { Literal, ThisExpression } from '../api/definition/values.js';
+import { UnaryExpression } from '../api/operators/unary.js';
+import { EmptyStatement } from '../api/statement/control/empty.js';
+import { BlockStatement } from '../api/statement/control/block.js';
+import { CatchClauseNode, ThrowStatement, TryCatchNode } from '../api/computing/throw.js';
+
+
+export interface SourcePositionFactory {
+	createSourcePosition(range?: [number, number]): SourceLocation | undefined;
+	rangeOfExpressionStatement(list: ExpressionNode[]): [number, number] | undefined;
+
+}
+
+export interface NodeFactory {
+	createDebuggerStatement(range?: [number, number]): DebuggerStatement;
+	createSuper(range?: [number, number]): Super;
+	createThis(range?: [number, number]): ThisExpression;
+	createNull(range?: [number, number]): Literal<null>;
+	createTrue(range?: [number, number]): Literal<true>;
+	createFalse(range?: [number, number]): Literal<false>;
+	createVoidZero(argument: ExpressionNode, range?: [number, number]): UnaryExpression;
+	createEmptyStatement(range?: [number, number]): EmptyStatement;
+	createExpressionStatement(list: ExpressionNode[]): ExpressionNode;
+	createCatchClause(block: BlockStatement, identifier?: ExpressionNode, range?: [number, number]): CatchClauseNode;
+	createTryStatement(tryBlock: BlockStatement, catchBlock?: ExpressionNode, finallyBlock?: ExpressionNode, range?: [number, number]): TryCatchNode;
+	createThrowStatement(exception: ExpressionNode, range?: [number, number]): ThrowStatement;
+}
