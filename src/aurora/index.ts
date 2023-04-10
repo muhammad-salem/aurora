37c477807f5eb3a3e74538cfc8086b6806c0045d
salemebo <salem.ebo@gmail.com>
2023-04-10 08:41:50 +0200

    update project references and dependencies

--- src/aurora/index.ts
+++ src/aurora/index.ts
@@ -1,5 +1,7 @@
+export * from '@ibyar/decorators';
 export * from '@ibyar/elements';
 export * from '@ibyar/expressions';
 export * from '@ibyar/core';
+export * from '@ibyar/platform';
 export * from '@ibyar/directives';
 export * from '@ibyar/pipes';
