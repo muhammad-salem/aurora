71126b775b02daa54236834ca9c46a4d70e6f346
salemebo <salem.ebo@gmail.com>
2024-04-22 08:37:30 +0200

    add effectProposal

---
+++ src/core/signals/proposal-signals.ts
@@ -0,0 +1,37 @@
+import { Signal } from 'signal-polyfill';
+
+let needsEnqueue = true;
+
+const w = new Signal.subtle.Watcher(() => {
+	if (needsEnqueue) {
+		needsEnqueue = false;
+		queueMicrotask(processPending);
+	}
+});
+
+function processPending() {
+	needsEnqueue = true;
+
+	for (const s of w.getPending()) {
+		s.get();
+	}
+
+	w.watch();
+}
+
+export function effectProposal(callback: () => (() => void) | void) {
+	let cleanup: (() => void) | void;
+
+	const computed = new Signal.Computed(() => {
+		typeof cleanup === 'function' && cleanup();
+		cleanup = callback();
+	});
+
+	w.watch(computed);
+	// computed.get();
+
+	return () => {
+		w.unwatch(computed);
+		typeof cleanup === 'function' && cleanup();
+	};
+}
