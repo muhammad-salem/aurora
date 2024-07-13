42e45e3948af12b6f68744ec860e1c924a9a23c6
muhammad-salem <salem.ebo@gmail.com>
2024-07-13 11:53:19 +0300

    add InjectionToken for dependance provider

--- src/core/di/inject.ts
+++ src/core/di/inject.ts
@@ -1,12 +1,24 @@
 import { TypeOf } from '../utils/typeof.js';
 
-const instances = new WeakMap<TypeOf<any>, any>();
+export class InjectionToken<T> {
+	constructor(public token: string) { }
+}
 
-export function inject<T>(type: TypeOf<T>) {
-	if (instances.has(type)) {
+type Provider<T> = TypeOf<T> | InjectionToken<T>;
+
+const instances = new WeakMap<Provider<any>, any>();
+
+export function inject<T>(type: TypeOf<T>): T;
+export function inject<T>(type: InjectionToken<T>): T | undefined;
+export function inject<T>(type: Provider<T>): T | undefined {
+	if (instances.has(type) || type instanceof InjectionToken) {
 		return instances.get(type);
 	}
 	const ref = new type();
 	instances.set(type, ref);
 	return ref;
+}
+
+export function provide<T>(provider: Provider<T>, value: T) {
+	instances.set(provider, value);
 }
