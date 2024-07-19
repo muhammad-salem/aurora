99f000d5a4e7bca981e1485957e96fc23db2bb0c
muhammad-salem <salem.ebo@gmail.com>
2024-07-19 11:47:37 +0300

    update helpers

--- src/cli/transformer/helpers.ts
+++ src/cli/transformer/helpers.ts
@@ -1,4 +1,5 @@
 import ts from 'typescript/lib/tsserverlibrary.js';
+import { InputOutputTypeInfo } from './modules.js';
 
 
 /**
@@ -22,6 +23,10 @@ export function isDirectiveDecorator(decorator: ts.Decorator, directiveName = 'D
 	return ts.isCallExpression(decorator.expression) && decorator.expression.expression.getText() === directiveName;
 };
 
+export function isComponentOrDirectiveDecorator(decorator: ts.Decorator, componentName = 'Component', directiveName = 'Directive'): boolean {
+	return isComponentDecorator(decorator, componentName) || isDirectiveDecorator(decorator, directiveName);
+};
+
 
 /**
  * check if a decorator is a `@Pipe` decorator;
@@ -43,3 +48,109 @@ export function isInjectableDecorator(decorator: ts.Decorator, injectableName = 
 	return ts.isCallExpression(decorator.expression) && decorator.expression.expression.getText() === injectableName;
 };
 
+
+/**
+ * check if Decorator is `@Input` or `@FormValue`
+ * @param decorator 
+ * @returns 
+ */
+export function isInputDecorator(decorator: ts.Decorator): boolean {
+	return ts.isCallExpression(decorator.expression) && (
+		decorator.expression.expression.getText() === 'Input'
+		|| decorator.expression.expression.getText() === 'FormValue'
+	);
+}
+
+/**
+ * check if Decorator is `@Output`
+ * @param decorator 
+ * @returns 
+ */
+export function isOutputDecorator(decorator: ts.Decorator): boolean {
+	return ts.isCallExpression(decorator.expression) && decorator.expression.expression.getText() === 'Output';
+}
+
+export function getMapByDecorator(classNode: ts.ClassDeclaration, checker: ts.TypeChecker, decoratorFilter: ((decorator: ts.Decorator) => boolean)): InputOutputTypeInfo {
+	const map: InputOutputTypeInfo = {};
+	classNode.members.forEach(member => {
+		if (!ts.isPropertyDeclaration(member)) {
+			return;
+		}
+		const decorators = ts.getDecorators(member);
+		if (!decorators) {
+			return;
+		}
+		const inputDecorators = decorators.filter(decoratorFilter);
+		if (!inputDecorators.length) {
+			return;
+		}
+		let inputType = member.type?.getText();
+		if (!inputType && member.initializer) {
+			inputType = checker.typeToString(checker.getTypeAtLocation(member.initializer), member.initializer, undefined);
+		}
+		inputDecorators.forEach(input => {
+			const decoratorCall = input.expression as ts.CallExpression;
+			const aliasName = decoratorCall.arguments[0] as ts.StringLiteralLike;
+			const inputName = aliasName ? aliasName.text : member.name.getText();
+			map[inputName] = inputType;
+		});
+	})
+	return map;
+}
+
+/**
+ * get a component or directive inputs that is annotated by `@Input` or `@FormValue`.
+ * @param classNode 
+ * @param checker 
+ * @returns 
+ */
+export function getInputs(classNode: ts.ClassDeclaration, checker: ts.TypeChecker): InputOutputTypeInfo {
+	return getMapByDecorator(classNode, checker, isInputDecorator);
+}
+
+/**
+ * get a component or directive input names that is annotated by `@Input` or `@FormValue`.
+ * @param classNode 
+ * @param checker 
+ * @returns 
+ */
+export function getInputNames(classNode: ts.ClassDeclaration, checker: ts.TypeChecker): string[] {
+	return Object.keys(getMapByDecorator(classNode, checker, isInputDecorator));
+}
+
+/**
+ * get a component or directive outputs that is annotated by `@Output`.
+ * @param classNode 
+ * @param checker 
+ * @returns 
+ */
+export function getOutputs(classNode: ts.ClassDeclaration, checker: ts.TypeChecker): InputOutputTypeInfo {
+	return getMapByDecorator(classNode, checker, isOutputDecorator);
+}
+
+/**
+ * get a component or directive output names that is annotated by `@Output`.
+ * @param classNode 
+ * @param checker 
+ * @returns 
+ */
+export function getOutputNames(classNode: ts.ClassDeclaration, checker: ts.TypeChecker): string[] {
+	return Object.keys(getMapByDecorator(classNode, checker, isOutputDecorator));
+}
+
+/**
+ * get text value for a property in ObjectLiteralExpression
+ * @param option 
+ * @param property 
+ * @returns 
+ */
+export function getTextValueForProperty(option: ts.ObjectLiteralExpression, property: string): string | undefined {
+	const selectorProperty = option.properties
+		.find(prop => prop.name?.getText() === property) as ts.PropertyAssignment | undefined;
+
+	const initializer = selectorProperty?.initializer.getText();
+	if (!initializer) {
+		return;
+	}
+	return initializer?.substring(1, initializer.length - 1);
+}
