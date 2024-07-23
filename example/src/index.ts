98acc1f10b8fd08c27db49386a8061564f7a2867
muhammad-salem <salem.ebo@gmail.com>
2024-07-23 21:54:29 +0300

    import @ibyar/aurora, so it can register directives

--- example/src/index.ts
+++ example/src/index.ts
@@ -2,6 +2,7 @@ import './polyfills.js'
 import './reactive-scope/scope.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import '@ibyar/aurora';
 
 export * from './extends/inherits.js'
 
