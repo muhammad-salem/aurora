c1b2d8f4674a3c6ea1ce23cec1455b3ba6c9d9b9
muhammad-salem <salem.ebo@gmail.com>
2024-07-27 12:30:22 +0300

    import modules first, then export all

--- src/aurora/index.ts
+++ src/aurora/index.ts
@@ -1,3 +1,11 @@
+import '@ibyar/decorators';
+import '@ibyar/elements';
+import '@ibyar/expressions';
+import '@ibyar/core';
+import '@ibyar/platform';
+import '@ibyar/directives';
+import '@ibyar/pipes';
+
 export * from '@ibyar/decorators';
 export * from '@ibyar/elements';
 export * from '@ibyar/expressions';
