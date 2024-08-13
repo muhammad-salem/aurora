8df7606c35cad22736d99cc77e659b2cb938d1a8
muhammad-salem <salem.ebo@gmail.com>
2024-08-13 07:17:04 +0300

    update example disabledFeatures

--- example/src/color/host-color.component.ts
+++ example/src/color/host-color.component.ts
@@ -3,6 +3,7 @@ import { Component, HostBinding, HostListener } from '@ibyar/aurora';
 @Component({
 	selector: 'host-color',
 	extend: 'div',
+	disabledFeatures: ['internals', 'shadow'],
 	template: `
 	<div>Click on color name, to change host: background color</div>
 	<div *for="let color of colors" data-color="{{color}}">
