e0300f00ebf2edb4c4a76243008ecb911c7ca426
muhammad-salem <salem.ebo@gmail.com>
2024-10-31 08:00:20 +0300

    fix getProvider

--- src/core/di/inject.ts
+++ src/core/di/inject.ts
@@ -4,21 +4,25 @@ import { InjectionProvider, InjectionToken, Provider } from './provider.js';
 
 const ROOT_PROVIDER = new InjectionProvider();
 
-const providers: InjectionProvider[] = [];
+const PROVIDERS: InjectionProvider[] = [ROOT_PROVIDER];
 
 export function forkProvider(): InjectionProvider {
 	return ROOT_PROVIDER.fork();
 }
 
 export function addProvider(modelProvider: InjectionProvider) {
-	providers.push(modelProvider);
+	PROVIDERS.push(modelProvider);
 }
 
 export function removeProvider(modelProvider: InjectionProvider) {
-	const index = providers.findIndex(provider => provider === modelProvider);
+	const index = PROVIDERS.findIndex(provider => provider === modelProvider);
 	if (index >= 0) {
-		providers.splice(index, 1);
+		PROVIDERS.splice(index, 1);
 	}
+}
+
+function getProvider(): InjectionProvider {
+	return PROVIDERS.at(-1) ?? ROOT_PROVIDER;
 }
 
 export function provide<T>(type: Type<T>): void;
@@ -27,18 +31,12 @@ export function provide<T>(type: Type<T>, value: T): void;
 export function provide<T>(type: AbstractType<T>, value: T): void;
 export function provide<T>(token: InjectionToken<T>, value: T): void;
 export function provide<T>(provider: Provider<T>, value?: T) {
-	ROOT_PROVIDER.provide(provider, value);
+	getProvider().provide(provider, value);
 }
 
 export function inject<T>(type: Type<T>): T;
 export function inject<T>(type: AbstractType<T>): T;
 export function inject<T>(token: InjectionToken<T>): T | undefined;
 export function inject<T>(provider: Provider<T>): T | undefined {
-	return ROOT_PROVIDER.inject(provider);
-}
-
-export function clearInjection<T>(type: Type<T>): void;
-export function clearInjection<T>(token: InjectionToken<T>): void;
-export function clearInjection<T>(provider: Provider<T>) {
-	ROOT_PROVIDER.clear(provider);
+	return getProvider().inject(provider);
 }
