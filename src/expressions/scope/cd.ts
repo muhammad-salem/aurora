6aeb8bc5f27a0426687811310809f90ad469faa7
muhammad-salem <salem.ebo@gmail.com>
2024-11-29 04:25:34 +0200

    refactor: use weak map

--- src/expressions/scope/cd.ts
+++ src/expressions/scope/cd.ts
@@ -1,11 +1,11 @@
-
-const SKIP_CHANGE_DETECTION = Symbol('NO_CD');
-
+const SKIP_CHANGE_DETECTION = new WeakMap<object, boolean>();
 
 export function skipChangeDetection(obj: object) {
-	Reflect.set(obj, SKIP_CHANGE_DETECTION, true);
+	if (obj !== null && typeof obj === 'object') {
+		SKIP_CHANGE_DETECTION.set(obj, true);
+	}
 }
 
 export function isChangeDetectionSkipped(obj: object) {
-	return Reflect.get(obj, SKIP_CHANGE_DETECTION) === true;
+	return obj !== null && typeof obj === 'object' && SKIP_CHANGE_DETECTION.has(obj);
 }
