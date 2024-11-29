9e2104a27ae53feffae7541b8b90bdc7d2765fec
muhammad-salem <salem.ebo@gmail.com>
2024-11-29 04:20:38 +0200

    add DirectiveViewContext

--- src/core/directive/directive.ts
+++ src/core/directive/directive.ts
@@ -1,10 +1,12 @@
 
+import { isSignal } from '@ibyar/expressions';
 import { HTMLComponent } from '../component/custom-element.js';
 import { inject } from '../di/inject.js';
 import { InjectionToken } from '../di/provider.js';
 import { TemplateRef } from '../linker/template-ref.js';
 import { ViewContainerRef } from '../linker/view-container-ref.js';
 import { AuroraZone } from '../zone/zone.js';
+import { isOutputSignal } from '../component/initializer.js';
 
 export const NATIVE_HOST_TOKEN = new InjectionToken<HTMLElement>('NATIVE_HOST');
 export const DIRECTIVE_HOST_TOKEN = new InjectionToken<HTMLComponent<any> | StructuralDirective>('DIRECTIVE_HOST');
@@ -32,4 +34,33 @@ export class StructuralDirective {
 export class AttributeDirective {
 	protected el = inject(NATIVE_HOST_TOKEN)!;
 	protected zone = inject(AuroraZone);
+}
+
+
+export class DirectiveViewContext {
+
+	static createContext(directive: StructuralDirective | AttributeDirective): DirectiveViewContext {
+		const ctx = new DirectiveViewContext(directive);
+		Object.entries(directive).forEach(([name, value]) => {
+			if (isSignal(value)) {
+				Object.defineProperty(ctx, name, {
+					get(): any {
+						return value.get();
+					},
+					set(newValue: any) {
+						return value.set(newValue);
+					},
+					enumerable: true,
+				});
+			} else if (isOutputSignal(value)) {
+				Reflect.set(ctx, name, value);
+			}
+		});
+		return ctx;
+	}
+
+	constructor(public readonly _directive: StructuralDirective | AttributeDirective) {
+
+	}
+
 }
