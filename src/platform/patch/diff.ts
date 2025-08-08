f2a2d2fc112730554f88a4b691118ca0efad5731
muhammad-salem <salem.ebo@gmail.com>
2025-08-08 12:19:19 +0300

    simplify patch diff in memory array

--- src/platform/patch/diff.ts
+++ src/platform/patch/diff.ts
@@ -1,117 +1,83 @@
 export type TrackBy<T, R = T> = (index: number, item: T) => R;
 export type DiffOptions<T, R = T> = { trackBy?: TrackBy<T, R> };
 
+
+/**
+ * in memory patch operations
+ */
 export enum PatchOperation {
 	ADD = 'add',
 	REMOVE = 'remove',
-	REPLACE = 'replace',
 	MOVE = 'move',
-	KEEP = 'keep'
 };
 
 export type PatchArray<T> = { currentIndex: number, nextIndex: number, op: PatchOperation, item: T };
 export type PatchObject = { key: string, op: PatchOperation };
-export type PatchReplaceRoot = { op: PatchOperation.REPLACE, root: true };
 export type DiffPatch<T> = PatchArray<T> | PatchObject;
 
 export class JSONPatch {
 
-	protected diffArray<T, R = T>(input: T[], output: T[], options?: DiffOptions<T, R>): PatchArray<T>[] | [PatchReplaceRoot] {
-		type DiffArray = PatchArray<T> & { ignore?: boolean };
+	protected diffArray<T, R = T>(input: T[], output: T[], options?: DiffOptions<T, R>): PatchArray<T>[] {
 		const trackBy = options?.trackBy;
-		const identityInput = (trackBy && input.map((item, index) => trackBy(index, item))) ?? input;
-		const identityOutput = (trackBy && output.map((item, index) => trackBy(index, item))) ?? output;
-		const diffArray: DiffArray[] = [];
-		identityInput.forEach((item, index) => {
-			const nextIndex = identityOutput.indexOf(item as T & R);
-			const op: PatchOperation = nextIndex == -1
-				? PatchOperation.REMOVE
-				: index == nextIndex
-					? PatchOperation.KEEP
-					: PatchOperation.MOVE;
-			diffArray.push({
-				currentIndex: index,
-				nextIndex: nextIndex,
-				op: op,
-				item: output[nextIndex]
-			});
-		});
-		identityOutput.forEach((item, index) => {
-			const oldIndex = identityInput.indexOf(item as T & R);
-			if (oldIndex == -1) {
-				diffArray.push({
-					currentIndex: -1,
-					nextIndex: index,
-					op: PatchOperation.ADD,
-					item: output[index]
-				});
+		const identityInput = (trackBy && input.map((item, index) => trackBy(index, item))) ?? input.slice() as unknown as R[];
+		const identityOutput = (trackBy && output.map((item, index) => trackBy(index, item))) ?? output.slice() as unknown as R[];
+		const diff: PatchArray<T>[] = [];
+		for (let currentIndex = identityInput.length - 1; currentIndex >= 0; currentIndex--) {
+			const item = identityInput[currentIndex];
+			const nextIndex = identityOutput.indexOf(item);
+			if (nextIndex !== -1) {
+				continue;
 			}
-		});
-		const patchArray: DiffArray[] = new Array<DiffArray>(output.length);
-		let i = 0;
-		diffArray.forEach(item => {
-			if (item.ignore) {
+			diff.push({
+				nextIndex,
+				currentIndex,
+				op: PatchOperation.REMOVE,
+				item: input[currentIndex]
+			});
+			identityInput.splice(currentIndex, 1);
+		}
+		identityOutput.forEach((item, nextIndex) => {
+			const currentIndex = identityInput.indexOf(item);
+			if (currentIndex === nextIndex) {
 				return;
 			}
-			let patch = item;
-			if (PatchOperation.REMOVE == item.op) {
-				const replacedItem = diffArray.find(r => !r.ignore && r.nextIndex == item.currentIndex);
-				if (replacedItem) {
-					if (PatchOperation.MOVE == replacedItem.op) {
-						item.ignore = replacedItem.ignore = true;
-						return;
-					} else {
-						replacedItem.ignore = true;
-						patch = {
-							currentIndex: item.currentIndex,
-							nextIndex: replacedItem.nextIndex,
-							op: PatchOperation.REPLACE,
-							item: replacedItem.item
-						};
-					}
-				}
+			const op = currentIndex === -1 ? PatchOperation.ADD : PatchOperation.MOVE;
+			diff.push({
+				op,
+				nextIndex,
+				currentIndex,
+				item: output[nextIndex]
+			});
+			if (op === PatchOperation.MOVE) {
+				identityInput.splice(currentIndex, 1);
 			}
-			patchArray[i++] = patch;
+			identityInput.splice(nextIndex, 0, item);
 		});
-		const removed = patchArray
-			.filter(item => !item.ignore && PatchOperation.REMOVE == item.op)
-			.sort((a, b) => b.currentIndex - a.currentIndex);
-		const apply = patchArray
-			.filter(item => !item.ignore && PatchOperation.REMOVE != item.op)
-			.sort((a, b) => a.nextIndex - b.nextIndex);
-		return removed.concat(apply) as PatchArray<T>[];
+		return diff;
 	}
 
-	protected diffObject<T = any>(input: T, output: T): PatchObject[] | [PatchReplaceRoot] {
+	protected diffObject<T = any>(input: T, output: T): PatchObject[] {
 		return [];
 	}
 
-	diff<T = any>(input: T, output: T): PatchObject[] | [PatchReplaceRoot];
-	diff<T>(input: T[], output: T[]): PatchArray<T>[] | [PatchReplaceRoot];
-	diff<T, R>(input: T[], output: T[], options?: DiffOptions<T, R>): PatchArray<T>[] | [PatchReplaceRoot];
-	diff(input: any, output: any, options?: DiffOptions<any, any>): DiffPatch<any>[] | [PatchReplaceRoot] {
+	diff<T = any>(input: T, output: T): PatchObject[];
+	diff<T>(input: T[], output: T[]): PatchArray<T>[];
+	diff<T, R>(input: T[], output: T[], options?: DiffOptions<T, R>): PatchArray<T>[];
+	diff(input: any, output: any, options?: DiffOptions<any, any>): DiffPatch<any>[] {
 		if (Array.isArray(input) && Array.isArray(output)) {
 			return this.diffArray(input, output, options);
 		} else if (typeof input == 'object' && typeof output == 'object') {
 			return this.diffObject(input, output);
 		}
-		return [{ op: PatchOperation.REPLACE, root: true }];
+		throw new Error('Not supported operation');
 	}
 }
 
 const jsonPatch = new JSONPatch();
 
-export function diff<T = any>(input: T, output: T): PatchObject[] | [PatchReplaceRoot];
-export function diff<T>(input: T[], output: T[]): PatchArray<T>[] | [PatchReplaceRoot];
-export function diff<T, R>(input: T[], output: T[], options?: DiffOptions<T, R>): PatchArray<T>[] | [PatchReplaceRoot];
-export function diff(input: any, output: any, options?: DiffOptions<any, any>): DiffPatch<any>[] | [PatchReplaceRoot] {
+export function diff<T = any>(input: T, output: T): PatchObject[];
+export function diff<T>(input: T[], output: T[]): PatchArray<T>[];
+export function diff<T, R>(input: T[], output: T[], options?: DiffOptions<T, R>): PatchArray<T>[];
+export function diff(input: any, output: any, options?: DiffOptions<any, any>): DiffPatch<any>[] {
 	return jsonPatch.diff(input, output, options);
-}
-
-export function isPatchReplaceRoot(patch: any): patch is PatchReplaceRoot {
-	let test: any = patch;
-	if (Array.isArray(patch)) {
-		test = patch[0];
-	}
-	return typeof test === 'object' && test.op === PatchOperation.REPLACE && test.root === true;
 }
